d955eb46c2f33277308cfebb85e2ca12f32f0137
dan <dan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2004-11-05 18:01:50 +0000

    Introduce some (temporary!) globals

--- branches/pluggable_encodings/charset/iso-8859-1.c
+++ branches/pluggable_encodings/charset/iso-8859-1.c
@@ -20,6 +20,10 @@ This file implements the charset functions for iso-8859-1 data
 /* The encoding we prefer, given a choice */
 static ENCODING *preferred_encoding;
 
+extern ENCODING *Parrot_fixed_8_encoding_ptr;
+
+CHARSET *Parrot_iso_8859_1_charset_ptr;
+
 static STRING *get_graphemes(Interp *interpreter, STRING *source_string, UINTVAL offset, UINTVAL count) {
     return ENCODING_GET_BYTES(interpreter, source_string, offset, count);
 }
@@ -244,9 +248,12 @@ CHARSET *Parrot_charset_iso_8859_1_init(Interp *interpreter) {
       find_word_boundary
   };
 
-  preferred_encoding = Parrot_load_encoding(interpreter, "fixed_8");
-  
+  /* Snag the global. This is... bad. Should be properly fixed at some
+     point */
+  preferred_encoding = Parrot_fixed_8_encoding_ptr;
+
   memcpy(return_set, &base_set, sizeof(CHARSET));
+  Parrot_iso_8859_1_charset_ptr = return_set;
   return return_set;
   
 }
