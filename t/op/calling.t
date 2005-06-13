parrot-mirror/parrot

commit 80a3393c7713b81a0c6ec72b5ced71e70d20a594

var_args 6 - skip the get_params
* if get_params is the first opcode of a sub, then stuff all
  arguments directly into the callee's register frame
* sequent get_params *will* still be processed to allow 'views',
  which are 'cool' (Autrijus). This will allow to get the passed
  arguments to a possibly different set of registers or apply some
  conversion like *@args or some such.



git-svn-id: https://svn.parrot.org/parrot/trunk@8345 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

classes/sub.pmc
@@ -61,7 +61,7 @@ print_sub_name(Interp* interpreter, PMC* sub)
     print_pbc_location(interpreter);
 }
 
-static void
+static opcode_t *
 copy_args_from_caller(Interp *interpreter, struct Parrot_sub * sub,
         struct parrot_regs_t *caller_regs)
 {
@@ -113,6 +113,7 @@ copy_args_from_caller(Interp *interpreter, struct Parrot_sub * sub,
 
     /* invalidate */
     interpreter->current_args = NULL;
+    return param_pc + n;
 }
 
 /*
@@ -283,6 +284,7 @@ Invokes the subroutine.
         struct parrot_regs_t *caller_regs;
         PMC *ccont;
         struct PackFile_ByteCode * old_seg;
+        opcode_t *pc;
 
         if (Interp_trace_TEST(INTERP, PARROT_TRACE_SUB_CALL_FLAG)) {
             print_sub_name(INTERP, SELF);
@@ -355,10 +357,11 @@ Invokes the subroutine.
          * new frame, e.g.
          * called from runops_fromc
          */
+        pc = sub->address;
         if (next) {
             /* XXX temporary hack during call syntax switch */
             if (INTERP->current_args) {
-                copy_args_from_caller(INTERP, sub, caller_regs);
+                pc = copy_args_from_caller(INTERP, sub, caller_regs);
             }
             else {
                 /* copy function arguments */
@@ -372,7 +375,7 @@ is_tail_call:
             INTERP->ctx.current_sub = SELF;
         }
         INTERP->ctx.current_HLL = sub->HLL_id;
-        return sub->address;
+        return pc;
     }
 
 /*

commit 2bd4408885afa587389fd0a9e9e80d27130c0f0e

var_args 7 - return integers from a sub
* get return results back to caller
* INTVALs only for now
* fix silly tail_call breakage from r8345

classes/continuation.pmc
@@ -22,6 +22,8 @@ See the L<Glossary|docs/glossary.pod> for more information.
 
 
 #include "parrot/parrot.h"
+#include "parrot/oplib/ops.h"
+#include <assert.h>
 
 
 /*
@@ -43,6 +45,54 @@ See the L<Glossary|docs/glossary.pod> for more information.
  * To avoid deep recursion during marking of nested subroutines, we
  * need the next_for_GC pointer in the pmc_ext area.
  */
+
+static void
+copy_args_from_callee(Interp *interpreter,
+        struct parrot_regs_t *caller_regs)
+{
+    /*  returns           results */
+    PMC *args_signature, *param_signature;
+    INTVAL i, n, sig;
+    opcode_t *pc, *param_pc;
+    struct PackFile_Constant **constants;
+    INTVAL i_arg;
+    FLOATVAL f_arg;
+    PMC *p_arg;
+    STRING *s_arg;
+
+    constants = interpreter->code->const_table->constants;
+    /* check if there is a get_params */
+    param_pc = interpreter->current_results;  /* XXX ctx. */
+    assert(*param_pc == PARROT_OP_get_results_pc);
+    param_signature = constants[param_pc[1]]->u.key;
+    assert(PObj_is_PMC_TEST(param_signature));
+    assert(param_signature->vtable->base_type == enum_class_FixedIntegerArray);
+
+    /* we point to the set_return opcode */
+    pc = interpreter->current_returns;
+    assert(*pc == PARROT_OP_set_returns_pc);
+    args_signature = constants[pc[1]]->u.key;
+    assert(PObj_is_PMC_TEST(args_signature));
+    assert(args_signature->vtable->base_type == enum_class_FixedIntegerArray);
+
+    /* never get more then the caller expects */
+    n = VTABLE_elements(interpreter, param_signature);
+
+    for (i = 0, pc += 2, param_pc += 2; i < n; ++i) {
+        /* fetch from args aka returns */
+        sig = VTABLE_get_integer_keyed_int(interpreter, args_signature, i);
+        switch (sig & PARROT_ARG_TYPE_MASK) {
+            case PARROT_ARG_INTVAL:
+                i_arg = pc[i];
+                if (!(sig & PARROT_ARG_CONSTANT)) {
+                    i_arg = BP_REG_INT(caller_regs, i_arg);
+                }
+                /* TODO verify param_signature */
+                REG_INT(param_pc[i]) = i_arg;
+                break;
+        }
+    }
+}
 
 pmclass Continuation need_ext {
 
@@ -207,8 +257,14 @@ destination to continue execution.
             PIO_eprintf(INTERP, "# Back in sub '%Ss'\n",
                 Parrot_full_sub_name(INTERP, sub));
         }
-        if (cc->address)
-            copy_regs(INTERP, caller_regs);
+        if (cc->address) {
+            if (INTERP->current_results) {      /* XXX ctx. */
+                copy_args_from_callee(INTERP, caller_regs);
+            }
+            else {
+                copy_regs(INTERP, caller_regs);
+            }
+        }
         else if (!cc->seg) {
             /* no address and no segment */
             return NULL;

classes/sub.pmc
@@ -94,6 +94,9 @@ copy_args_from_caller(Interp *interpreter, struct Parrot_sub * sub,
     assert(args_signature->vtable->base_type == enum_class_FixedIntegerArray);
 
 
+    /* TODO factor out common bits with copy_from_callee in
+     * classes/continuation.pmc
+     */
     n = VTABLE_elements(interpreter, args_signature);
 
     for (i = 0, pc += 2, param_pc += 2; i < n; ++i) {
@@ -109,7 +112,6 @@ copy_args_from_caller(Interp *interpreter, struct Parrot_sub * sub,
                 break;
         }
     }
-
 
     /* invalidate */
     interpreter->current_args = NULL;
@@ -338,6 +340,7 @@ Invokes the subroutine.
          *   impact on possible continuations executing later here.
          *
          */
+        pc = sub->address;
         if (!PMC_IS_NULL(ccont)) {
             if (PObj_get_FLAGS(ccont) & SUB_FLAG_TAILCALL) {
                 PObj_get_FLAGS(ccont) &= ~SUB_FLAG_TAILCALL;
@@ -357,7 +360,6 @@ Invokes the subroutine.
          * new frame, e.g.
          * called from runops_fromc
          */
-        pc = sub->address;
         if (next) {
             /* XXX temporary hack during call syntax switch */
             if (INTERP->current_args) {

t/op/calling.t
@@ -16,7 +16,7 @@ Tests Parrot calling conventions.
 
 =cut
 
-use Parrot::Test tests => 5;
+use Parrot::Test tests => 6;
 use Test::More;
 
 # Test calling convention operations
@@ -200,3 +200,33 @@ CODE
 77
 back
 OUTPUT
+
+output_is(<<'CODE', <<'OUTPUT', "call - i, ic, return i, ic");
+.pcc_sub main:
+    set I16, 77
+    set_args "(0b100, 0)", 42, I16
+    get_results "(0, 0)", I16, I17
+    find_name P1, "foo"
+    invokecc P1
+    print I16
+    print "\n"
+    print I17
+    print "\nback\n"
+    end
+.pcc_sub foo:
+    get_params "(0, 0)", I16, I17
+    print I16
+    print "\n"
+    print I17
+    print "\n"
+    set I16, 88
+    set_returns "(4, 0)", 99, I16
+    returncc
+CODE
+42
+77
+99
+88
+back
+OUTPUT
+