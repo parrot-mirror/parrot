5be565772752eb9267554d414e82676f7ad63abd
heimdall <heimdall@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2005-08-09 00:54:13 +0000

    Initialization functions for gmc and generations.

--- branches/gmc/src/gc_gmc.c
+++ branches/gmc/src/gc_gmc.c
@@ -2,10 +2,106 @@
 
 #if PARROT_GC_GMC
 
-/*static void gc_gmc_pool_init(Interp *interpreter, struct Small_Object_Pool *pool) 
-{
 
-}*/
+static void gc_gmc_add_free_object(Interp*, struct Small_Object_Pool*, void*);
+static void *gc_gmc_get_free_object(Interp*, struct Small_Object_Pool*);
+static void gc_gmc_alloc_objects(Interp*, struct Small_Object_Pool*);
+static void gc_gmc_more_objects(Interp*, struct Small_Object_Pool*);
+
+
+/* Allocates and initializes a generation, but does not plug it to the pool yet. */
+static Gc_gmc_gen *
+gc_gmc_gen_init(Interp *interpreter)
+{
+    Gc_gmc_gen *gen;
+    Gc_gmc_hdr_list *IGP;
+    Gc_gmc_hdr_store *IGP_store;
+
+    gen = mem_sys_allocate(sizeof(Gc_gmc_gen));
+    gen->first = mem_sys_allocate(GMC_GEN_SIZE);
+    
+    /* And fill the blanks. */
+    gen->fst_free = gen->first;
+    gen->last = NULL;
+
+    /* We have an IGP basis : only one store. */
+    IGP_store = mem_sys_allocate(sizeof(Gc_gmc_hdr_store));
+    IGP_store->ptr = &(IGP_store->store[0]);
+    IGP_store->next = NULL;
+    IGP = mem_sys_allocate(sizeof(Gc_gmc_hdr_list));
+    IGP->first = IGP_store;
+    IGP->last = IGP_store;
+    gen->IGP = IGP;
+    
+    return gen;
+}
+
+/* Inserts the given generation to the right place, keeping all generation 
+ * sorted (by insertion sort). */
+static void
+gc_gmc_insert_gen(Interp *interpreter, struct Small_Object_Pool *pool, Gc_gmc_gen *gen)
+{
+    Gc_gmc_gen *cur_gen;
+
+    cur_gen = pool->gc->last;
+
+    /* Find the right place for the address malloc gave us. */
+    while ((UINTVAL)cur_gen->first > (UINTVAL)gen->first)
+      cur_gen = cur_gen->prev;
+    
+    /* Insert the generation. */
+    if (cur_gen)
+    {
+      if (cur_gen->next)
+      {
+	cur_gen->next->prev = gen;
+      }
+      cur_gen->next = gen;
+      gen->next = cur_gen->next;
+    } else {
+      gen->next = NULL;
+      pool->gc->first = gen;
+      pool->gc->last = gen;
+    }
+    gen->prev = cur_gen;
+    if (pool->gc->last == cur_gen)
+      pool->gc->last = gen;
+}
+
+
+static void 
+gc_gmc_pool_init(Interp *interpreter, struct Small_Object_Pool *pool) 
+{
+    struct Arenas *arena_base;
+    Gc_gmc *gc;
+    Gc_gmc_gen *gen;
+    int i;
+    
+    pool->add_free_object = gc_gmc_add_free_object;
+    pool->get_free_object = gc_gmc_get_free_object;
+    pool->alloc_objects   = gc_gmc_alloc_objects;
+    pool->more_objects    = gc_gmc_more_objects;
+
+    gc = mem_sys_allocate(sizeof(Gc_gmc));
+    
+    gc->nb_gen = GMC_GEN_INIT_NUMBER;
+    gc->nb_empty_gen = GMC_GEN_INIT_NUMBER;
+    gc->alloc_obj = 0;
+    gc->first = NULL;
+    gc->fst_free = NULL;
+    gc->lst_free = NULL;
+    gc->last = NULL;
+    gc->timely = gc_gmc_gen_init(interpreter);
+    gc->constant = gc_gmc_gen_init(interpreter);
+    pool->gc = gc;
+    
+    for (i = 0; i < GMC_GEN_INIT_NUMBER; i++)
+    {
+	gen = gc_gmc_gen_init(interpreter);
+	gc_gmc_insert_gen(interpreter, pool, gen);
+    }
+}
+
 
 void gc_gmc_deinit(Interp *interpreter)
 {
@@ -15,11 +111,11 @@ void gc_gmc_deinit(Interp *interpreter)
 void gc_gmc_run(Interp *interpreter, int flags)
 {
 #ifdef GMC_DEBUG
-  fprintf (stderr, "Trying to run dod_run\n");
+  fprintf (stderr, "GMC: Trying to run dod_run\n");
 #endif /* GMC_DEBUG */
 }
 
-/*void Parrot_gc_gmc_init(Interp *interpreter)
+void Parrot_gc_gmc_init(Interp *interpreter)
 {
   struct Arenas *arena_base;
 
@@ -28,29 +124,50 @@ void gc_gmc_run(Interp *interpreter, int flags)
   arena_base->do_dod_run = gc_gmc_run;
   arena_base->de_init_gc_system = gc_gmc_deinit;
   arena_base->init_pool = gc_gmc_pool_init;
-}*/
+}
 
 
-/* Small adaptation of gc_ms_free_object. Does not yet use GMC at all. */
+/******************************* FAKE THINGS ********************************/
 
 void *
 gc_gmc_get_free_object(Interp *interpreter,
-    struct Small_Object_Pool *pool)
+	struct Small_Object_Pool *pool)
 {
-    void *ptr;
-
-    /* if we don't have any objects */
-    if (!pool->free_list)
-        (*pool->more_objects) (interpreter, pool);
-    ptr = pool->free_list;
-    pool->free_list = *(void **)ptr;
-    --pool->num_free_objects;
-#if ! DISABLE_GC_DEBUG
-    if (GC_DEBUG(interpreter))
-        PObj_version((Buffer*)ptr) = interpreter->arena_base->dod_runs;
-#endif
-    return ptr;
+    return NULL;
 }
+
+void 
+gc_gmc_add_free_object(Interp *interpreter,
+	struct Small_Object_Pool *pool, void *to_add)
+{
+#ifdef GMC_DEBUG
+    fprintf (stderr, "GMC: Adding object %p to the free list\n", to_add);
+#endif
+}
+
+void
+gc_gmc_alloc_objects(Interp *interpreter,
+	struct Small_Object_Pool *pool)
+{
+#ifdef GMC_DEBUG
+    fprintf (stderr, "GMC: Allocating more objects\n");
+#endif
+}
+
+void
+gc_gmc_more_objects(Interp *interpreter,
+	struct Small_Object_Pool *pool)
+{
+#ifdef GMC_DEBUG
+    fprintf (stderr, "GMC: I want more objects !\n");
+#endif
+}
+
+
+
+
+/******************************* REAL THINGS ********************************/
+
 
 
 /* The real thing, but not plugged yet */
@@ -68,8 +185,6 @@ gc_gmc_real_get_free_object(Interp *interpreter,
   /* TODO: find a way to know if we want an aggregate allocation or not.
    * For now, let's say that everything is non-aggregate. */
   
-  
-
   return ptr;
 }
 
