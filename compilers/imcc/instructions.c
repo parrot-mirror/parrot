parrot-mirror/parrot

commit 1a53be3ddaeb88f5a457e84357358baf6b4e2c83

Register allocator - returncc, tailcall, get_results, ...
* returncc and tailcall don't branch to the next basic block, like
  e.g a conditional branch - set branch flags accordingly
* better life-range handling for get_results:
  while the get_results opcode comes before a sub call, the effect
  WRT life analysis happens only after the call - postpone
  get_results' effect after the call 
* fix a bogus test (t/examples/sub_2) - it was expectiong obviously
  wrong results due to earlier register alligator errors


git-svn-id: https://svn.parrot.org/parrot/trunk@11402 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

compilers/imcc/instructions.c
@@ -228,8 +228,24 @@ instruction_writes(Instruction* ins, SymReg* r) {
 
     f = ins->flags;
 
-    if (ins->opnum == PARROT_OP_get_params_pc ||
-            ins->opnum == PARROT_OP_get_results_pc) {
+    /* 
+     * a get_results opcode is before the actual sub call
+     * but for the register allocator, the effect matters, thus
+     * postpone the effect after the invoke
+     */
+    if (ins->opnum == PARROT_OP_get_results_pc) 
+        return 0;
+    else if (ins->prev && (ins->prev->type & ITPCCSUB)) {
+        ins = ins->prev;
+        assert(ins->r[0]->pcc_sub);
+        for (i = 0; i < ins->r[0]->pcc_sub->nret; ++i) {
+            if (r == ins->r[0]->pcc_sub->ret[i])
+                return 1;
+        }
+        return 0;
+    }
+
+    if (ins->opnum == PARROT_OP_get_params_pc) {
         for (i = 0; i < ins->n_r; i++) {
             if (ins->r[i] == r)
                 return 1;

compilers/imcc/cfg.c
@@ -513,7 +513,8 @@ analyse_life_symbol(Parrot_Interp interpreter, IMC_Unit * unit, SymReg* r)
              */
             if (ins->prev) {
                 prev = ins->prev;
-                if (prev->type & (ITPCCSUB|ITPCCYIELD))
+                if ((prev->type & (ITPCCSUB|ITPCCYIELD)) &&
+                        prev->opnum != PARROT_OP_tailcall_p)
                     r->usage |= U_NON_VOLATILE;
                 else if (prev->opnum == PARROT_OP_invoke_p_p ||
                          prev->opnum == PARROT_OP_invokecc_p)
@@ -574,14 +575,9 @@ analyse_life_block(Parrot_Interp interpreter, Basic_block* bb, SymReg* r)
                     "Index %i of %i has NULL instruction\n",
                     ins->index, bb->end->index);
         }
-        if (ins->opnum == -1) {
-            if (ins == bb->end)
-                break;
-            continue;
-        }
         /*
          * if we have a setp_ind opcode, it may write all PMC
-         * registers from 5..15
+         * registers 
          */
         if (ins->opnum == PARROT_OP_setp_ind_i_p && r->set == 'P') {
             r->usage |= U_NON_VOLATILE;

compilers/imcc/parser_util.c
@@ -557,7 +557,10 @@ INS(Interp *interpreter, IMC_Unit * unit, char *name,
     }
     if (op_info->jump && op_info->jump != PARROT_JUMP_ENEXT) {
         ins->type |= ITBRANCH;
-        if (!strcmp(name, "branch"))
+        /* TODO use opnum constants */
+        if (!strcmp(name, "branch") ||
+            !strcmp(name, "tailcall") ||
+            !strcmp(name, "returncc"))    
             ins->type |= IF_goto;
         else if (!strcmp(fullname, "jump_i") ||
                 !strcmp(fullname, "jsr_i") ||

t/examples/subs.t
@@ -72,9 +72,9 @@ Hello from main
 END_EXPECTED
 
     'single_retval.pir'     =>  << 'END_EXPECTED',
-7 10 nine 10
+7 8 nine 10
 return: 10
-7 10 nine 10
+7 8 nine 10
 return: 10
 END_EXPECTED
 

commit 48359d8a9fb2a4ecb3d2d485f27e082f3ae8c897

Fix register allocation bug introduced in r11402
* the effect of get_results was one ins after invoke, instead at invoke
  this caused wrong register allocation, if this ins was part of the next
  basic block, i.e. after a label

Thanks to Coke for spotting the exact rev.

Fix register allocation with PASM regs & non-volatiles

* Non-volatile register didn't honor already pre-assigned PASM registers
  and thus could get the same register number as the PASM reg.

compilers/imcc/instructions.c
@@ -236,7 +236,7 @@ instruction_writes(Instruction* ins, SymReg* r) {
      */
     if (ins->opnum == PARROT_OP_get_results_pc) 
         return 0;
-    else if (ins->prev && (ins->prev->type & ITPCCSUB)) {
+    else if (ins->type & ITPCCSUB) {
         ins = ins->prev;
         /* can't used pcc_sub->ret due to bug #38406
          * it seems that all sub SymRegs are shared

compilers/imcc/reg_alloc.c
@@ -700,7 +700,7 @@ map_colors(IMC_Unit* unit, int x, unsigned int *graph, char avail[],
  */
 
 static int
-first_avail(IMC_Unit *unit, int reg_set)
+first_avail(IMC_Unit *unit, int reg_set, Set **avail)
 {
     int i, n, first;
     SymReg * r;
@@ -723,7 +723,10 @@ first_avail(IMC_Unit *unit, int reg_set)
         }
     }
     first = set_first_zero(allocated);
-    set_free(allocated);
+    if (avail)
+        *avail = allocated;
+    else
+        set_free(allocated);
     return first;
 }
 
@@ -739,11 +742,12 @@ allocate_uniq(Parrot_Interp interpreter, IMC_Unit *unit, int usage)
     int i, j, reg_set, first_reg;
     SymReg * r;
     SymHash *hsh;
+    Set *avail;
 
     hsh = &unit->hash;
     for (j = 0; j < 4; j++) {
         reg_set = type[j];
-        first_reg = first_avail(unit, reg_set);
+        first_reg = first_avail(unit, reg_set, &avail);
         for (i = 0; i < hsh->size; i++) {
             for (r = hsh->data[i]; r; r = r->next) {
                 if (r->set != reg_set)
@@ -751,6 +755,9 @@ allocate_uniq(Parrot_Interp interpreter, IMC_Unit *unit, int usage)
                 if ((r->type & VTREGISTER) &&
                         r->color == -1 &&
                         (r->usage & usage)) {
+                    if (set_contains(avail, first_reg))
+                        first_reg = first_avail(unit, reg_set, NULL);
+                    set_add(avail, first_reg);
                     r->color = first_reg++;
                     IMCC_debug(interpreter, DEBUG_IMC,
                             "allocate %s sym %c '%s'  color %d\n",
@@ -759,6 +766,7 @@ allocate_uniq(Parrot_Interp interpreter, IMC_Unit *unit, int usage)
                 }
             }
         }
+        set_free(avail);
         unit->first_avail[j] = first_reg;
     }
     /*