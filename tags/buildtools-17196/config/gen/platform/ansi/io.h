d2a201594a3414e485c32c0555c225d548164d3a
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-02-27 02:52:43 +0000

    Tagging most recent point in trunk so that buildtools branch may be synched to it.

---
+++ tags/buildtools-17196/config/gen/platform/ansi/io.h
@@ -0,0 +1,15 @@
+/*
+** I/O:
+*/
+
+#define DEFAULT_OPEN_MODE 0
+#ifndef S_ISREG
+#  define S_ISREG(m) 1
+#endif
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
