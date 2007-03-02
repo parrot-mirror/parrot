a99576e62c46bc8dbfa91c735fd2443074f34daf
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-03-02 15:27:14 +0000

    Tagging trunk at r17261 so that the kelvin can be synched to it.

---
+++ tags/kelvin-17261/config/gen/platform/openbsd/misc.h
@@ -0,0 +1,16 @@
+/*
+** Miscellaneous:
+*/
+
+#include <sys/exec.h>
+
+#ifdef NATIVE_EXEC_ELF
+#  define PARROT_OPENBSD_ELF
+#endif
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
