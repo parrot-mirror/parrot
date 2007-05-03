2662a5b18d6e5a5fb2e157f7842fc03cc41cffb4
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-05-03 00:39:59 +0000

    Tagging trunk at r18401 so that the reconfigure can be synched to it.

---
+++ tags/reconfigure-18401/compilers/pirc/src/pbcout.h
@@ -0,0 +1,15 @@
+#ifndef __PBCOUT_H
+#  define __PBCOUT_H
+
+
+extern struct pirvtable *init_pbc_vtable(char *outputfile);
+
+#endif
+
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
