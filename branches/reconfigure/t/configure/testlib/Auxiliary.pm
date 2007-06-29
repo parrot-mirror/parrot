7d931a0fa06b1a5822b27fc9128a225b6b189461
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-06-29 01:12:15 +0000

    Added POD.

--- branches/reconfigure/t/configure/testlib/Auxiliary.pm
+++ branches/reconfigure/t/configure/testlib/Auxiliary.pm
@@ -6,7 +6,7 @@ use warnings;
 our ( @ISA, @EXPORT_OK );
 @ISA       = qw(Exporter);
 @EXPORT_OK = qw(
-    test_hint
+    test_step_thru_runstep
 );
 use Carp;
 *ok = *Test::More::ok;
@@ -14,7 +14,7 @@ use Carp;
 use lib qw( . lib ../lib ../../lib );
 use Parrot::Configure;
 
-sub test_hint {
+sub test_step_thru_runstep {
     my ($conf, $pkg, $args, $stepnum) = @_;
     my ($task, $step_name, @step_params, $step, $ret);
     
@@ -34,3 +34,56 @@ sub test_hint {
 }
 
 1;
+
+################### DOCUMENTATION ###################
+
+=head1 NAME
+
+Auxiliary - subroutines used in F<t/configure/*> tests
+
+=head1 SYNOPSIS
+
+    use lib qw( . lib ../lib ../../lib t/configure/testlib );
+    use Auxiliary qw( test_step_thru_runstep );
+
+    $parrot_version = Parrot::BuildUtil::parrot_version();
+    $args = process_options( {
+    argv            => [ ],
+    script          => $0,
+    parrot_version  => $parrot_version,
+    svnid           => '$Id: 105-init_hints.01.t 19432 2007-06-29 00:24:16Z jkeenan $',
+} );
+
+    $conf = Parrot::Configure->new;
+    test_step_thru_runstep($conf, q{init::defaults}, $args, 0);
+
+=head1 DESCRIPTION
+
+The subroutines in this package are used to simplify tests found in
+F<t/configure/>.
+
+So far, only one subroutine is available for export on demand:
+C<test_step_thru_runstep()>.  This subroutine takes as arguments the
+Parrot::Configure object, a string holding the name of the step to be run,
+hashref C<$args> which is the output of C<process_options()> and an integer
+which, so far, must be manually incremented between invocations of the
+subroutine.
+
+Each invocation of C<test_step_thru_runstep()> runs 4 tests.
+
+=head1 AUTHOR
+
+David H Adler and James E Keenan
+
+=head1 SEE ALSO
+
+F<Configure.pl>.
+
+=cut
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
