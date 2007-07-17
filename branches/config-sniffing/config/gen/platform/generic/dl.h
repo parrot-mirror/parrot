3f5b404122b46a2d0b06cccca7ecb29e62c14783
petdance <petdance@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-07-17 02:12:35 +0000

    Making a branch for the work to make Configure.pl sniff the capabilities of the compiler it has to work with.

---
+++ branches/config-sniffing/config/gen/platform/generic/dl.h
@@ -0,0 +1,16 @@
+#ifndef PARROT_PLATFORM_GENERIC_DL_H_GUARD
+#define PARROT_PLATFORM_GENERIC_DL_H_GUARD
+/*
+ * Dynamic loading stuff:
+ */
+
+#define PARROT_DLOPEN_FLAGS RTLD_LAZY
+
+#endif /* PARROT_PLATFORM_GENERIC_DL_H_GUARD */
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
