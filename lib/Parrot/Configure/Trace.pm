5d40fd10230e3dbf92fa80e7755539065af7d3f1
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-07-21 15:02:44 +0000

    Introducing Parrot::Configure::Trace.  Trace the evolution of the Parrot::Configure object's data structure over the course of the configuration steps.

---
+++ lib/Parrot/Configure/Trace.pm
@@ -0,0 +1,278 @@
+# Copyright (C) 2001-2005, The Perl Foundation.
+# $Id: Data.pm 19800 2007-07-11 23:38:47Z jkeenan $
+
+package Parrot::Configure::Trace;
+use strict;
+use warnings;
+use Data::Dumper;
+use Storable qw(nstore retrieve);
+
+sub new {
+    my $class = shift;
+    my $argsref = shift || {};
+    my $self = bless( [], $class );
+    my $sto = $argsref->{storable} || q{.configure_trace.sto};
+    @{$self} = @{ retrieve($sto) };
+    return $self;
+}
+
+sub list_steps {
+    my $self = shift;
+    return $self->[0];
+}
+
+sub index_steps {
+    my $self = shift;
+    my @steps = @{ $self->list_steps() };
+    my %index = ();
+    for (my $i = 0; $i <= $#steps; $i++) {
+        $index{$steps[$i]} = $i + 1;
+    }
+    return \%index;
+}
+
+sub trace_data_c {
+    my ($self, $argsref) = @_;
+    my @data = @{$self};
+    my @c = ();
+    for (my $step = 1; $step <= $#data; $step++) {
+        my $value = $data[$step]->{data}->{c}->{$argsref->{attr}};
+        if ($argsref->{verbose}) {
+            push @c, { $self->[0]->[$step - 1] => $value };
+        } else {
+            push @c, $value;
+        }
+    }
+    return \@c;
+}
+
+sub trace_data_triggers {
+    my ($self, $argsref) = @_;
+    my @data = @{$self};
+    my @triggers = ();
+    for (my $step = 1; $step <= $#data; $step++) {
+        my $value =
+            $data[$step]->{data}->{triggers}->{$argsref->{trig}};
+        if ($argsref->{verbose}) {
+            push @triggers, { $self->[0]->[$step - 1] => $value };
+        } else {
+            push @triggers, $value;
+        }
+    }
+    return \@triggers;
+}
+
+################### DOCUMENTATION ###################
+
+=head1 NAME
+
+Parrot::Configure::Trace - Trace development of Parrot::Configure object through the configuration steps
+
+=head1 SYNOPSIS
+
+When calling F<perl Configure.pl>:
+
+    $ perl Configure.pl --configure_trace
+
+After configuration has completed:
+
+    use Parrot::Configure::Trace;
+
+    $obj = Parrot::Configure::Trace->new();
+
+    $steps_list = $obj->list_steps();
+
+    $steps_index = $obj->index_steps();
+
+    $attr = $obj->trace_data_c( {
+        attr        => 'some_attr',
+        verbose     => 1,               # optional
+    } );
+
+    $attr = $obj->trace_data_triggers( {
+        trig        => 'some_trig',
+        verbose     => 1,               # optional
+    } );
+
+=head1 DESCRIPTION
+
+This module provides ways to trace the evolution of the data structure within
+the Parrot::Configure object over the various steps in the configuration
+process.  An understanding of this data structure's development may be useful
+to Parrot developers working on the configuration process or its results.
+
+To make use of its methods, first configure with the
+C<--configure_trace> option.  As configuration proceeds through what are
+currently 56 individual steps, the state of the Parrot::Configuration object
+is recorded in a Perl array reference stored on disk via the Storable module
+in a file called F<.configure_trace.sto> stored in the top-level of your
+Parrot sandbox directory.
+
+Once that storable file has been created, you can write programs which
+retrieve its data into a Parrot::Configure::Trace object and then call methods
+on that object.
+
+=head1 METHODS
+
+=head2 C<new()>
+
+    $obj = Parrot::Configure::Trace->new();
+
+=over 4
+
+=item * Purpose
+
+Parrot::Configure::Trace constructor.  Retrieve configuration data recorded on
+disk over the course of the configuration steps and populate a
+Parrot::Configure::Trace object with that data.
+
+=item * Arguments
+
+None currently required.  However, to provide for future extensibility, you
+may provide a reference to a hash in which various attributes are set which
+will affect the Parrot::Configure::Trace object.  Currently, the only such
+attribute is C<storable>, whose value is the name of the Storable file holding
+configuration data if that file is named something other than
+F<.configure_trace.sto>.
+
+=item * Return Value
+
+Parrot::Configure::Trace object.
+
+=item * Comment
+
+The Parrot::Configure::Trace object is a blessed array reference.  Element
+C<0> of that array is a reference to an array holding the names of the
+individual configuration steps; elements C<1> through C<$#array> hold the
+state of the Parrot::Configure object at the conclusion of each step.
+
+=back
+
+=head2 C<list_steps()>
+
+    $steps_list = $obj->list_steps();
+
+=over 4
+
+=item * Purpose
+
+Provide list of the names of the configuration steps.
+
+=item * Arguments
+
+None.
+
+=item * Return Value
+
+Array reference:
+
+    [
+        'init::manifest',
+        'init::defaults',
+        ...
+        'gen::config_pm'
+    ]
+
+=back
+
+=head2 C<index_steps()>
+
+    $steps_index = $obj->index_steps();
+
+=over 4
+
+=item * Purpose
+
+Provide lookup table showing which step number a given configuration step is.
+
+=item * Arguments
+
+None.
+
+=item * Return Value
+
+Hash reference:
+
+    {
+        'inter::ops'        => 19,
+        'init::optimize'    => 13,
+        ...
+        'init::defaults'    =>  2,
+    }
+
+=back
+
+=head2 C<trace_data_c()>
+
+=over 4
+
+=item * Purpose
+
+Provide a list of the values which a given attribute in the C<{data}->{c}>
+part of the Parrot::Configure object takes over the course of the
+configuration steps.
+
+=item * Arguments
+
+Hash reference.  Key C<attr> is mandatory; it is the key whose value you wish
+to trace over the course of the configuration steps.  Key C<verbose> is
+optional.
+
+=item * Return Value
+
+Array reference.  Element C<n> of this array holds the value of the attribute
+in the C<{data}->{c}> part of the Parrot::Configure object at configuration
+step C<n + 1>.
+
+If, however, C<verbose> is set, each element C<n> of the array holds a hash
+reference where the hash key is the name of configuration step C<n + 1> and
+the value is the value of the attribute at step C<n + 1>.
+
+=back
+
+=head2 C<trace_data_triggers()>
+
+=over 4
+
+=item * Purpose
+
+Provide a list of the values which a given attribute in the
+C<{data}->{triggers}> part of the Parrot::Configure object takes over the
+course of the configuration steps.
+
+=item * Arguments
+
+Hash reference.  Key C<attr> is mandatory; it is the key whose value you wish
+to trace over the course of the configuration steps.  Key C<verbose> is
+optional.
+
+=item * Return Value
+
+Array reference.  Element C<n> of this array holds the value of the attribute
+in the C<{data}->{triggers}> part of the Parrot::Configure object at
+configuration step C<n + 1>.
+
+If, however, C<verbose> is set, each element C<n> of the array holds a hash
+reference where the hash key is the name of configuration step C<n + 1> and
+the value is the value of the attribute at step C<n + 1>.
+
+=back
+
+=head1 AUTHOR
+
+James E Keenan (jkeenan@cpan.org)
+
+=head1 SEE ALSO
+
+L<Parrot::Configure>, L<Parrot::Configure::Options>, F<Configure.pl>.
+
+=cut
+
+1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
