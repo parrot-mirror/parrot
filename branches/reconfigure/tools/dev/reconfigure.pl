26e1df8dc9b29235be7b3a96587341f9b5dc5b12
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-07-31 01:25:48 +0000

    *Very preliminary* first draft of a program to re-run a configuration step without calling Configure.pl.

---
+++ branches/reconfigure/tools/dev/reconfigure.pl
@@ -0,0 +1,48 @@
+#! perl
+use 5.008_000;
+use strict;
+use warnings;
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+use lib 'lib';
+
+use Parrot::BuildUtil;
+use Parrot::Configure;
+use Parrot::Configure::Options qw( process_options );
+use Parrot::Configure::Step::List qw( get_steps_list );
+
+
+my $parrot_version = Parrot::BuildUtil::parrot_version();
+$| = 1; # $OUTPUT_AUTOFLUSH = 1;
+my $args = process_options( {
+    argv            => [ $ARGV[0] ],
+    script          => $0,
+    parrot_version  => $parrot_version,
+    svnid           => '$Id$',
+} );
+exit(1) unless defined $args;
+
+my $conf = Parrot::Configure->new;
+$conf->add_steps(get_steps_list());
+$conf->options->set(%{$args});
+
+if ( exists $args->{step} ) {
+    $conf->data()->slurp();
+    $conf->data()->slurp_temp()
+        if $args->{step} =~ /gen::makefiles/;
+    $conf->run_single_step( $args->{step} );
+    print "\n";
+    exit(0);
+}
+else {
+    warn "tools/dev/reconfigure.pl only handles re-running of configuration steps (--step option)";
+    exit(1)
+}
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
+
