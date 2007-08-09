a6c015e921657a5ed00c183f9f2a5d9f153c324a
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-08-09 23:48:54 +0000

    Tagging trunk at r20586 so that the reconfigure can be synched to it.

---
+++ tags/reconfigure-20586/config/gen/platform/openbsd/misc.h
@@ -0,0 +1,20 @@
+#ifndef PARROT_PLATFORM_OPENBSD_MISC_H_GUARD
+#define PARROT_PLATFORM_OPENBSD_MISC_H_GUARD
+/*
+** Miscellaneous:
+*/
+
+#include <sys/exec.h>
+
+#ifdef NATIVE_EXEC_ELF
+#  define PARROT_OPENBSD_ELF
+#endif
+
+#endif /* PARROT_PLATFORM_OPENBSD_MISC_H_GUARD */
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
