7e1f83ff24d5752775a6eb3d62227f7fbb89fcfb
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-09-02 17:38:06 +0000

    Creating fixinterprogs in https://svn.perl.org/parrot/branches

---
+++ branches/fixinterprogs/config/gen/platform/win32/begin.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2004-2006, The Perl Foundation.
+ */
+
+#include <windows.h>
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
