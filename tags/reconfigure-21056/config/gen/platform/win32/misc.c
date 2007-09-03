22816cdcac9af0cb1300656af750bf2ff552fdab
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-09-03 17:52:40 +0000

    Tagging trunk at r21056 so that the reconfigure can be synched to it.

---
+++ tags/reconfigure-21056/config/gen/platform/win32/misc.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2004-2006, The Perl Foundation.
+ */
+
+
+void
+Parrot_platform_init_code(void)
+{
+ SetErrorMode(SEM_NOGPFAULTERRORBOX);
+}
+
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
+
