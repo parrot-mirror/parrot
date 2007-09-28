9244293f96e9286f44645c07adcb6474de62b590
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-09-28 02:38:03 +0000

    First draft of implementation of --fatal-step option to Configure.pl.  User will be able to supply a comma-delimited string of names of configuration steps which, upon failure (undef return), will cause Configure.pl to halt completely.

--- lib/Parrot/Configure/Options/Conf.pm
+++ lib/Parrot/Configure/Options/Conf.pm
@@ -29,6 +29,7 @@ our @valid_options = qw{
     define
     exec-prefix
     execcapable
+    fatal-step
     floatval
     gc
     help
@@ -122,6 +123,8 @@ General Options:
    --verbose=2          Output every setting change
    --verbose-step=N     Set verbose for step N only
    --verbose-step=regex Set verbose for step matching description
+   --fatal-step         Comma-delimited string of configuration steps
+                        which upon failure cause Configure.pl to halt
    --nomanicheck        Don't check the MANIFEST
    --step=(gen::languages)
                         Execute a single configure step
