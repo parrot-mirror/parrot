6fe1e9cd6e62525b39bb4c07c290664dd4b8803c
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-11-20 12:30:52 +0000

    Adding set, get and add methods to Parrot::Configure::Initialize.  Converting tests to use that.  Not stable.

--- branches/nointer/lib/Parrot/Configure/Initialize/Defaults.pm
+++ branches/nointer/lib/Parrot/Configure/Initialize/Defaults.pm
@@ -25,176 +25,326 @@ sub init_defaults {
 
 sub _init_from_options {
     my $self = shift;
-    $self->{debugging} = $self->{options}->{debugging} ? 1 : 0;
-    $self->{verbose}   = $self->{options}->{verbose};
+#    $self->{debugging} = $self->{options}->{debugging} ? 1 : 0;
+#    $self->{verbose}   = $self->{options}->{verbose};
+    $self->set(
+        debugging => $self->{options}->{debugging} ? 1 : 0,
+        verbose   => $self->{options}->{verbose},
+    );
 }
 
 sub _init_from_Config {
     my $self = shift;
-    # Compiler -- used to turn .c files into object files.
-    # (Usually cc or cl, or something like that.)
-    $self->{cc} = $Config{cc};
-    $self->{ccflags} = $Config{ccflags};
-    $self->{ccwarn} = exists( $Config{ccwarn} ) ? $Config{ccwarn} : '';
-
-    # Flags used to indicate this object file is to be compiled
-    # with position-independent code suitable for dynamic loading.
-    # e.g. -fpic for GNU cc.
-    $self->{cc_shared} = $Config{cccdlflags};
-
-    # C++ compiler -- used to compile parts of ICU.  ICU's configure
-    # will try to find a suitable compiler, but it prefers GNU c++ over
-    # a system c++, which might not be appropriate.  This setting
-    # allows you to override ICU's guess, but is otherwise currently
-    # unset.  Ultimately, it should be set to whatever ICU figures
-    # out, or parrot should look for it and always tell ICU what to
-    # use.
-    $self->{cxx} = 'c++';
-
-    # Linker, used to link object files (plus libraries) into
-    # an executable.  It is usually $cc on Unix-ish systems.
-    # VMS and Win32 might use "Link".
-    # Perl5's Configure doesn't distinguish linking from loading, so
-    # make a reasonable guess at defaults.
-    $self->{link} = $Config{cc};
-    $self->{linkflags} = $Config{ldflags};
-
-    # Linker Flags to have this binary work with the 
-    # shared and dynamically loadable libraries we're building.
-    # On HP-UX, for example, we need to
-    # allow dynamic libraries to access the binary's symbols
-    # e.g. -Wl,-E on HP-UX
-    $self->{link_dynamic} = $Config{ccdlflags};
-
-    # ld: Tool used to build shared libraries and dynamically loadable
-    # modules. Often $cc on Unix-ish systems, but apparently sometimes
-    # it's ld.
-    $self->{ld} = $Config{ld};
-    $self->{ldflags} = $Config{ldflags};
-
-    # Some operating systems (e.g. Darwin) distinguish between shared
-    # libraries and modules that can be dynamically loaded.
-    # Flags to tell ld to build a shared library, 
-    # e.g., -shared for GNU ld.
-    $self->{ld_share_flags} = $Config{lddlflags};
-
-    # Flags to tell ld to build a dynamically loadable module,
-    # e.g., -shared for GNU ld.
-    $self->{ld_load_flags} = $Config{lddlflags};
-
-    $self->{libs} = $Config{libs};
-
-    $self->{cc_inc} = "-I./include";
-    $self->{cc_debug} = '-g';
-    $self->{link_debug} = '';
-
-    $self->{o} = $Config{_o};       # object files extension
-    $self->{share_ext} = ".$Config{so}";    # shared library extension
-
-    # dynamically loadable module extension
-    $self->{load_ext} = ".$Config{so}";
-    $self->{a} = $Config{_a};        # library or archive extension
-    $self->{exe} = $Config{_exe};      # executable files extension
-    $self->{cc_o_out} = '-o ';              # cc object output file
-
-    # cc executable output file (different on Win32)
-    $self->{cc_exe_out} = '-o ';
-
-    # prefix for ldflags (necessary for Win32)
-    $self->{cc_ldflags} = '';
-
-    # ld output file.  Keep the trailing space.
-    $self->{ld_out} = '-o ';
-
-    # include debug info in executable
-    $self->{ld_debug} = '';
-
-    # Way to decorate a function to mark it as an exportable or
-    # importable symbol.
-    $self->{sym_export} = '';
-    $self->{sym_import} = '';
-
-    # Library build directory
-    $self->{blib_dir} = 'blib/lib';
-
-    # libparrot library names
-    $self->{libparrot_static} = 'libparrot' . $Config{_a};
-    $self->{libparrot_shared} = 'libparrot.' . $Config{so};
-
-    # does the system know about static/dynamic linking?
-    $self->{has_static_linking} = 1;
-    $self->{has_dynamic_linking} = 0;
-
-    # default behaviour for linking parrot to a 
-    # static or shared libparrot
-    $self->{parrot_is_shared} = 0;
-
-    #  avoid a warning during Configure.pl
-    $self->{libparrot_soname} = '';
-
-    $self->{perl} = $^X;
-    $self->{perl_inc} = _find_perl_headers();
-    $self->{test_prog} = 'parrot';
-    $self->{rm_f} = '$(PERL) -MExtUtils::Command -e rm_f';
-    $self->{rm_rf} = '$(PERL) -MExtUtils::Command -e rm_rf';
-    $self->{mkpath} = '$(PERL) -MExtUtils::Command -e mkpath';
-    $self->{touch} = '$(PERL) -MExtUtils::Command -e touch';
-    $self->{chmod} =
-        '$(PERL) -MExtUtils::Command -e ExtUtils::Command::chmod';
-    $self->{ar} = $Config{ar};
-    $self->{ar_flags} = 'cr';
-
-    # for Win32
-    $self->{ar_out} = '';
-
-    # for Borland C
-    $self->{ar_extra} = '';
-    $self->{ranlib} = $Config{ranlib};
-    $self->{rpath} = '';
-    $self->{make} = $Config{make};
-    $self->{make_set_make} = $Config{make_set_make};
-    $self->{make_and} = '&&';
-
-    # make_c: Command to emulate GNU make's C<-C directory> option:
-    # chdir to C<directory> before executing $(MAKE)
-    $self->{make_c} = '$(PERL) -e \'chdir shift @ARGV; system q{$(MAKE)}, @ARGV; exit $$? >> 8;\'';
-
-    # if platform has a .s file that needs to be assembled
-    $self->{platform_asm} = 0;
-    $self->{as} = 'as';    # assembler
-
-    $self->{cp} = '$(PERL) -MExtUtils::Command -e cp';
-    $self->{lns} = $Config{lns};                          # soft link
-    $self->{slash} = '/';
+#    # Compiler -- used to turn .c files into object files.
+#    # (Usually cc or cl, or something like that.)
+#    $self->{cc} = $Config{cc};
+#    $self->{ccflags} = $Config{ccflags};
+#    $self->{ccwarn} = exists( $Config{ccwarn} ) ? $Config{ccwarn} : '';
+#
+#    # Flags used to indicate this object file is to be compiled
+#    # with position-independent code suitable for dynamic loading.
+#    # e.g. -fpic for GNU cc.
+#    $self->{cc_shared} = $Config{cccdlflags};
+#
+#    # C++ compiler -- used to compile parts of ICU.  ICU's configure
+#    # will try to find a suitable compiler, but it prefers GNU c++ over
+#    # a system c++, which might not be appropriate.  This setting
+#    # allows you to override ICU's guess, but is otherwise currently
+#    # unset.  Ultimately, it should be set to whatever ICU figures
+#    # out, or parrot should look for it and always tell ICU what to
+#    # use.
+#    $self->{cxx} = 'c++';
+#
+#    # Linker, used to link object files (plus libraries) into
+#    # an executable.  It is usually $cc on Unix-ish systems.
+#    # VMS and Win32 might use "Link".
+#    # Perl5's Configure doesn't distinguish linking from loading, so
+#    # make a reasonable guess at defaults.
+#    $self->{link} = $Config{cc};
+#    $self->{linkflags} = $Config{ldflags};
+#
+#    # Linker Flags to have this binary work with the 
+#    # shared and dynamically loadable libraries we're building.
+#    # On HP-UX, for example, we need to
+#    # allow dynamic libraries to access the binary's symbols
+#    # e.g. -Wl,-E on HP-UX
+#    $self->{link_dynamic} = $Config{ccdlflags};
+#
+#    # ld: Tool used to build shared libraries and dynamically loadable
+#    # modules. Often $cc on Unix-ish systems, but apparently sometimes
+#    # it's ld.
+#    $self->{ld} = $Config{ld};
+#    $self->{ldflags} = $Config{ldflags};
+#
+#    # Some operating systems (e.g. Darwin) distinguish between shared
+#    # libraries and modules that can be dynamically loaded.
+#    # Flags to tell ld to build a shared library, 
+#    # e.g., -shared for GNU ld.
+#    $self->{ld_share_flags} = $Config{lddlflags};
+#
+#    # Flags to tell ld to build a dynamically loadable module,
+#    # e.g., -shared for GNU ld.
+#    $self->{ld_load_flags} = $Config{lddlflags};
+#
+#    $self->{libs} = $Config{libs};
+#
+#    $self->{cc_inc} = "-I./include";
+#    $self->{cc_debug} = '-g';
+#    $self->{link_debug} = '';
+#
+#    $self->{o} = $Config{_o};       # object files extension
+#    $self->{share_ext} = ".$Config{so}";    # shared library extension
+#
+#    # dynamically loadable module extension
+#    $self->{load_ext} = ".$Config{so}";
+#    $self->{a} = $Config{_a};        # library or archive extension
+#    $self->{exe} = $Config{_exe};      # executable files extension
+#    $self->{cc_o_out} = '-o ';              # cc object output file
+#
+#    # cc executable output file (different on Win32)
+#    $self->{cc_exe_out} = '-o ';
+#
+#    # prefix for ldflags (necessary for Win32)
+#    $self->{cc_ldflags} = '';
+#
+#    # ld output file.  Keep the trailing space.
+#    $self->{ld_out} = '-o ';
+#
+#    # include debug info in executable
+#    $self->{ld_debug} = '';
+#
+#    # Way to decorate a function to mark it as an exportable or
+#    # importable symbol.
+#    $self->{sym_export} = '';
+#    $self->{sym_import} = '';
+#
+#    # Library build directory
+#    $self->{blib_dir} = 'blib/lib';
+#
+#    # libparrot library names
+#    $self->{libparrot_static} = 'libparrot' . $Config{_a};
+#    $self->{libparrot_shared} = 'libparrot.' . $Config{so};
+#
+#    # does the system know about static/dynamic linking?
+#    $self->{has_static_linking} = 1;
+#    $self->{has_dynamic_linking} = 0;
+#
+#    # default behaviour for linking parrot to a 
+#    # static or shared libparrot
+#    $self->{parrot_is_shared} = 0;
+#
+#    #  avoid a warning during Configure.pl
+#    $self->{libparrot_soname} = '';
+#
+#    $self->{perl} = $^X;
+#    $self->{perl_inc} = _find_perl_headers();
+#    $self->{test_prog} = 'parrot';
+#    $self->{rm_f} = '$(PERL) -MExtUtils::Command -e rm_f';
+#    $self->{rm_rf} = '$(PERL) -MExtUtils::Command -e rm_rf';
+#    $self->{mkpath} = '$(PERL) -MExtUtils::Command -e mkpath';
+#    $self->{touch} = '$(PERL) -MExtUtils::Command -e touch';
+#    $self->{chmod} =
+#        '$(PERL) -MExtUtils::Command -e ExtUtils::Command::chmod';
+#    $self->{ar} = $Config{ar};
+#    $self->{ar_flags} = 'cr';
+#
+#    # for Win32
+#    $self->{ar_out} = '';
+#
+#    # for Borland C
+#    $self->{ar_extra} = '';
+#    $self->{ranlib} = $Config{ranlib};
+#    $self->{rpath} = '';
+#    $self->{make} = $Config{make};
+#    $self->{make_set_make} = $Config{make_set_make};
+#    $self->{make_and} = '&&';
+#
+#    # make_c: Command to emulate GNU make's C<-C directory> option:
+#    # chdir to C<directory> before executing $(MAKE)
+#    $self->{make_c} = '$(PERL) -e \'chdir shift @ARGV; system q{$(MAKE)}, @ARGV; exit $$? >> 8;\'';
+#
+#    # if platform has a .s file that needs to be assembled
+#    $self->{platform_asm} = 0;
+#    $self->{as} = 'as';    # assembler
+#
+#    $self->{cp} = '$(PERL) -MExtUtils::Command -e cp';
+#    $self->{lns} = $Config{lns};                          # soft link
+#    $self->{slash} = '/';
+    $self->set(
+        # Compiler -- used to turn .c files into object files.
+        # (Usually cc or cl, or something like that.)
+        cc => $Config{cc},
+        ccflags => $Config{ccflags},
+        ccwarn => exists( $Config{ccwarn} ) ? $Config{ccwarn} : '',
+    
+        # Flags used to indicate this object file is to be compiled
+        # with position-independent code suitable for dynamic loading.
+        # e.g. -fpic for GNU cc.
+        cc_shared => $Config{cccdlflags},
+    
+        # C++ compiler -- used to compile parts of ICU.  ICU's configure
+        # will try to find a suitable compiler, but it prefers GNU c++ over
+        # a system c++, which might not be appropriate.  This setting
+        # allows you to override ICU's guess, but is otherwise currently
+        # unset.  Ultimately, it should be set to whatever ICU figures
+        # out, or parrot should look for it and always tell ICU what to
+        # use.
+        cxx => 'c++',
+    
+        # Linker, used to link object files (plus libraries) into
+        # an executable.  It is usually $cc on Unix-ish systems.
+        # VMS and Win32 might use "Link".
+        # Perl5's Configure doesn't distinguish linking from loading, so
+        # make a reasonable guess at defaults.
+        link => $Config{cc},
+        linkflags => $Config{ldflags},
+    
+        # Linker Flags to have this binary work with the 
+        # shared and dynamically loadable libraries we're building.
+        # On HP-UX, for example, we need to
+        # allow dynamic libraries to access the binary's symbols
+        # e.g. -Wl,-E on HP-UX
+        link_dynamic => $Config{ccdlflags},
+    
+        # ld: Tool used to build shared libraries and dynamically loadable
+        # modules. Often $cc on Unix-ish systems, but apparently sometimes
+        # it's ld.
+        ld => $Config{ld},
+        ldflags => $Config{ldflags},
+    
+        # Some operating systems (e.g. Darwin) distinguish between shared
+        # libraries and modules that can be dynamically loaded.
+        # Flags to tell ld to build a shared library, 
+        # e.g., -shared for GNU ld.
+        ld_share_flags => $Config{lddlflags},
+    
+        # Flags to tell ld to build a dynamically loadable module,
+        # e.g., -shared for GNU ld.
+        ld_load_flags => $Config{lddlflags},
+    
+        libs => $Config{libs},
+    
+        cc_inc => "-I./include",
+        cc_debug => '-g',
+        link_debug => '',
+    
+        o => $Config{_o},       # object files extension
+        share_ext => ".$Config{so}",    # shared library extension
+    
+        # dynamically loadable module extension
+        load_ext => ".$Config{so}",
+        a => $Config{_a},        # library or archive extension
+        exe => $Config{_exe},      # executable files extension
+        cc_o_out => '-o ',              # cc object output file
+    
+        # cc executable output file (different on Win32)
+        cc_exe_out => '-o ',
+    
+        # prefix for ldflags (necessary for Win32)
+        cc_ldflags => '',
+    
+        # ld output file.  Keep the trailing space.
+        ld_out => '-o ',
+    
+        # include debug info in executable
+        ld_debug => '',
+    
+        # Way to decorate a function to mark it as an exportable or
+        # importable symbol.
+        sym_export => '',
+        sym_import => '',
+    
+        # Library build directory
+        blib_dir => 'blib/lib',
+    
+        # libparrot library names
+        libparrot_static => 'libparrot' . $Config{_a},
+        libparrot_shared => 'libparrot.' . $Config{so},
+    
+        # does the system know about static/dynamic linking?
+        has_static_linking => 1,
+        has_dynamic_linking => 0,
+    
+        # default behaviour for linking parrot to a 
+        # static or shared libparrot
+        parrot_is_shared => 0,
+    
+        #  avoid a warning during Configure.pl
+        libparrot_soname => '',
+    
+        perl => $^X,
+        perl_inc => _find_perl_headers(),
+        test_prog => 'parrot',
+        rm_f => '$(PERL) -MExtUtils::Command -e rm_f',
+        rm_rf => '$(PERL) -MExtUtils::Command -e rm_rf',
+        mkpath => '$(PERL) -MExtUtils::Command -e mkpath',
+        touch => '$(PERL) -MExtUtils::Command -e touch',
+        chmod =>
+            '$(PERL) -MExtUtils::Command -e ExtUtils::Command::chmod',
+        ar => $Config{ar},
+        ar_flags => 'cr',
+    
+        # for Win32
+        ar_out => '',
+    
+        # for Borland C
+        ar_extra => '',
+        ranlib => $Config{ranlib},
+        rpath => '',
+        make => $Config{make},
+        make_set_make => $Config{make_set_make},
+        make_and => '&&',
+    
+        # make_c: Command to emulate GNU make's C<-C directory> option:
+        # chdir to C<directory> before executing $(MAKE)
+        make_c => '$(PERL) -e \'chdir shift @ARGV, system q{$(MAKE)}, @ARGV; exit $$? >> 8;\'',
+    
+        # if platform has a .s file that needs to be assembled
+        platform_asm => 0,
+        as => 'as',    # assembler
+    
+        cp => '$(PERL) -MExtUtils::Command -e cp',
+        lns => $Config{lns},                          # soft link
+        slash => '/',
+    );
 }
 
 sub _init_version {
     my $self = shift;
     my $parrot_version = Parrot::BuildUtil::parrot_version();
     my @parrot_version = Parrot::BuildUtil::parrot_version();
-    $self->{VERSION} = $parrot_version;
-    $self->{MAJOR} = $parrot_version[0];
-    $self->{MINOR} = $parrot_version[1];
-    $self->{PATCH} = $parrot_version[2];
-    $self->{DEVEL} = ( -e 'DEVELOPING' ? '-devel' : '' );
+#    $self->{VERSION} = $parrot_version;
+#    $self->{MAJOR} = $parrot_version[0];
+#    $self->{MINOR} = $parrot_version[1];
+#    $self->{PATCH} = $parrot_version[2];
+#    $self->{DEVEL} = ( -e 'DEVELOPING' ? '-devel' : '' );
+    $self->set(
+        VERSION => $parrot_version,
+        MAJOR => $parrot_version[0],
+        MINOR => $parrot_version[1],
+        PATCH => $parrot_version[2],
+        DEVEL => ( -e 'DEVELOPING' ? '-devel' : '' ),
+    );
 }
 
 sub _init_misc {
     my $self = shift;
-    $self->{optimize}   = '';
-    $self->{build_dir}  = abs_path($FindBin::Bin);
-    $self->{configdate} = scalar gmtime() . " GMT";
-    $self->{PQ} = "'";
-    $self->{dquote} = "\\\"";
+    $self->set(
+        optimize => '',
+        build_dir => abs_path($FindBin::Bin),
+        configdate => scalar gmtime() . " GMT",
+        PQ => "'",
+        dquote => "\\\"",
 
-    # yacc = Automatic parser generator
-    # lex  = Automatic lexer  generator
-    # Some systems may lack these
-    $self->{yacc} = 'bison -v -y';
-    $self->{lex} = 'flex';
+        # yacc = Automatic parser generator
+        # lex  = Automatic lexer  generator
+        # Some systems may lack these
+        yacc => 'bison -v -y',
+        lex => 'flex',
 
-    # Extra flags needed for libnci_test.so
-    $self->{ncilib_link_extra} = '';
+        # Extra flags needed for libnci_test.so
+        ncilib_link_extra => '',
+    );
 }
 
 sub _init_profile {
@@ -204,8 +354,10 @@ sub _init_profile {
     # we should have this in the hints files e.g. cc_profile
     # RT#41496 move profiling to it's own step
     if ( $self->{options}->{profile} ) {
-        $self->{cc_debug} = " -pg ";
-        $self->{ld_debug} = " -pg ";
+        $self->set(
+            cc_debug => " -pg ",
+            ld_debug => " -pg ",
+        );
     }
 }
 
@@ -221,7 +373,7 @@ sub _init_archname {
 
             # adjust gcc?
             for my $cc qw(cc cxx link ld) {
-                $self->{$cc} .= '-m32';
+                $self->add( ' ', $cc, '-m32' );
             }
 
             # and lib flags
@@ -231,15 +383,15 @@ sub _init_archname {
                 ldflags
                 linkflags
             ) {
-                my $item = $self->{$lib};
+                my $item = $self->get($lib);
                 ( my $ni = $item ) =~ s/lib64/lib/g;
-                $self->{$lib} = $ni;
+                $self->set( $lib => $ni );
             }
         }
     }
     # RT#41500 adjust lib install-path /lib64 vs. lib
     # remember corrected archname - jit.pm was using $Config('archname')
-    $self->{archname} = $archname;
+    $self->set( archname => $archname);
 }
 
 sub _find_perl_headers {
