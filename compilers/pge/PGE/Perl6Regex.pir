parrot-mirror/parrot

commit 4d490d9f45d9cdaabc6b6d6f09d2dfe479e67ee2

[pge]: * Add "** 2..4" quantifier syntax, and tests. * Patches courtesy Jonathan Scott Duff <perlpilot@gmail.com>  PerlJam++
git-svn-id: https://svn.parrot.org/parrot/trunk@22944 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

compilers/pge/PGE/Perl6Regex.pir
@@ -498,50 +498,15 @@ combinations.
     max = 1
     suffixpos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
 
-    #   The postfix:<::> operator may bring us here when it's really a
+    #   The postfix:<:> operator may bring us here when it's really a
     #   term:<::> term.  So, we check for that here and fail this match
     #   if we really have a cut term.
-    if key != ':' goto quant
+    if key != ':' goto quant_suffix
     $S0 = substr target, pos, 1
     if $S0 == ':' goto end
     mob['backtrack'] = PGE_BACKTRACK_NONE
     goto quant_suffix_1
 
-  quant:
-    if key == '**' goto quant_closure
-    if key == '+' goto quant_max
-    min = 0
-  quant_max:
-    if key == '?' goto quant_suffix
-    max = PGE_INF
-    goto quant_suffix
-  quant_closure:
-    pos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
-    $S0 = substr target, pos, 1
-    if $S0 != "{" goto err_closure
-    inc pos
-    $I1 = find_not_cclass .CCLASS_NUMERIC, target, pos, lastpos
-    if $I1 <= pos goto err_closure
-    $S0 = substr target, pos
-    min = $S0
-    max = $S0
-    pos = $I1
-    $S0 = substr target, pos, 2
-    if $S0 != '..' goto quant_closure_end
-    pos += 2
-    max = PGE_INF
-    $S0 = substr target, pos, 1
-    if $S0 == '.' goto quant_closure_end
-    $I1 = find_not_cclass .CCLASS_NUMERIC, target, pos, lastpos
-    if $I1 <= pos goto err_closure
-    $S0 = substr target, pos
-    max = $S0
-    pos = $I1
-  quant_closure_end:
-    $S0 = substr target, pos, 1
-    if $S0 != "}" goto err_closure
-    inc pos
-    suffixpos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
   quant_suffix:
     suffix = substr target, suffixpos, 2
     if suffix == ':?' goto quant_eager
@@ -550,7 +515,7 @@ combinations.
     suffix = substr target, suffixpos, 1
     if suffix == '?' goto quant_eager
     if suffix == '!' goto quant_greedy
-    if suffix != ':' goto quant_set
+    if suffix != ':' goto quant
   quant_none:
     mob['backtrack'] = PGE_BACKTRACK_NONE
     goto quant_skip_suffix
@@ -562,6 +527,53 @@ combinations.
   quant_skip_suffix:
     $I0 = length suffix
     pos = suffixpos + $I0
+
+  quant:
+    if key == '**' goto quant_closure
+    if key == '+' goto quant_max
+    min = 0
+  quant_max:
+    if key == '?' goto quant_set
+    max = PGE_INF
+    goto quant_set
+
+  quant_closure:
+    pos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
+    .local int isconst
+    isconst = is_cclass .CCLASS_NUMERIC, target, pos
+    if isconst goto brace_skip
+    $S0 = substr target, pos, 1
+    if $S0 != "{" goto err_closure
+    inc pos
+  brace_skip:
+    $I1 = find_not_cclass .CCLASS_NUMERIC, target, pos, lastpos
+    if $I1 <= pos goto err_closure
+    $S0 = substr target, pos
+    min = $S0
+    max = $S0
+    pos = $I1
+    $S0 = substr target, pos, 2
+    if $S0 != '..' goto quant_closure_end
+    pos += 2
+    max = PGE_INF
+    $S0 = substr target, pos, 1
+    if $S0 != '*' goto quant_range_end
+    inc pos
+    goto quant_closure_end
+  quant_range_end:
+    $I1 = find_not_cclass .CCLASS_NUMERIC, target, pos, lastpos
+    if $I1 <= pos goto err_closure
+    $S0 = substr target, pos
+    max = $S0
+    pos = $I1
+  quant_closure_end:
+    if isconst goto brace_skip2
+    $S0 = substr target, pos, 1
+    if $S0 != "}" goto err_closure
+    inc pos
+  brace_skip2:
+    suffixpos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
+
   quant_set:
     mob['min'] = min
     mob['max'] = max

MANIFEST
@@ -1,7 +1,7 @@
 # ex: set ro:
 # $Id$
 #
-# generated by tools/dev/mk_manifest_and_skip.pl Wed Nov 21 23:00:45 2007 UT
+# generated by tools/dev/mk_manifest_and_skip.pl Thu Nov 22 00:01:38 2007 UT
 #
 # See tools/dev/install_files.pl for documentation on the
 # format of this file.
@@ -2925,6 +2925,7 @@ t/compilers/pge/perl6regex/rx_charclass                     []
 t/compilers/pge/perl6regex/rx_lookarounds                   []
 t/compilers/pge/perl6regex/rx_metachars                     []
 t/compilers/pge/perl6regex/rx_modifiers                     []
+t/compilers/pge/perl6regex/rx_quantifiers                   []
 t/compilers/pge/perl6regex/rx_subrules                      []
 t/compilers/pge/perl6regex/rx_syntax                        []
 t/compilers/pge/pge-hs.t                                    []

t/compilers/pge/perl6regex/01-regex.t
@@ -73,6 +73,7 @@ Description of the test.
 
     # populate the list of test files
     push test_files, 'rx_metachars'
+    push test_files, 'rx_quantifiers'
     push test_files, 'rx_backtrack'
     push test_files, 'rx_charclass'
     push test_files, 'rx_subrules'

t/compilers/pge/perl6regex/rx_quantifiers
@@ -1,0 +1,88 @@
+##  Quantifiers
+a*				bbbbb		y	zero "a" characters
+a*				aaaaa		y	five "a" characters
+a+				bbabb		y	one "a" character
+a+				aaaaa		y	five "a" characters
+a+				bbbbb		n	no "a" characters
+a?				bbbbb		y	zero "a" characters
+a?				bbabb		y	one "a" character
+a?				baaab		y	one "a" character
+
+a*!				bbbbb		y	zero "a" characters (explicit greed)
+a*!				aaaaa		y	five "a" characters (explicit greed)
+a+!				bbabb		y	one "a" character (explicit greed)
+a+!				aaaaa		y	five "a" characters (explicit greed)
+a+!				bbbbb		n	no "a" characters (explicit greed)
+a?!				bbbbb		y	zero "a" characters (explicit greed)
+a?!				bbabb		y	one "a" character (explicit greed)
+a?!				baaab		y	one "a" character (explicit greed)
+
+a*:!			bbbbb		y	zero "a" characters (explicit greed)
+a*:!			aaaaa		y	five "a" characters (explicit greed)
+a+:!			bbabb		y	one "a" character (explicit greed)
+a+:!			aaaaa		y	five "a" characters (explicit greed)
+a+:!			bbbbb		n	no "a" characters (explicit greed)
+a?:!			bbbbb		y	zero "a" characters (explicit greed)
+a?:!			bbabb		y	one "a" character (explicit greed)
+a?:!			baaab		y	one "a" character (explicit greed)
+
+a*?				bbbbb		y	zero "a" characters (non-greedy)
+a*?				aaaaa		y	zero "a" characters (non-greedy)
+a+?				bbabb		y	one "a" character (non-greedy)
+a+?				aaaaa		y	one "a" character (non-greedy)
+a+?				bbbbb		n	no "a" characters (non-greedy)
+a??				bbbbb		y	zero "a" characters (non-greedy)
+a??				bbabb		y	zero "a" characters (non-greedy)
+a??				baaab		y	zero "a" characters (non-greedy)
+
+a*:?			bbbbb		y	zero "a" characters (non-greedy)
+a+:?			bbabb		y	one "a" character (non-greedy)
+a+:?			aaaaa		y	one "a" character (non-greedy)
+a+:?			bbbbb		n	no "a" characters (non-greedy)
+a?:?			bbbbb		y	zero "a" characters (non-greedy)
+a?:?			bbabb		y	zero "a" characters (non-greedy)
+a?:?			baaab		y	zero "a" characters (non-greedy)
+
+## Quantifier closure
+.**{2}			a			n	only one character
+.**{2}			ab			y	two characters
+a**{2}			foobar		n	only one "a" character
+a**{2}			baabaa		y	two "a" characters
+a**{0..4}		bbbbbbb		y	no "a" characters
+a**{2..4}		bababab		n	not two consecutive "a" characters
+a**{2..4}		baabbbb		y	two "a" characters
+a**{2..4}		baaabbb		y	three "a" characters
+a**{2..4}		baaaabb		y	four "a" characters
+a**{2..4}		baaaaaa		y	four "a" characters
+a**{2..*}		baaaaaa		y	six "a" characters
+a**?{2..*}		baaaaaa		y	two "a" characters (non-greedy)
+a**:?{2..*}		baaaaaa		y	two "a" characters (non-greedy)
+a**!{2..*}		baaaaaa		y	six "a" characters (explicit greed)
+a**:!{2..*}		baaaaaa		y	six "a" characters (explicit greed)
+a**?{2..4}		baaabbb		y	two "a" characters (non-greedy)
+a**:?{2..4}		baaabbb		y	two "a" characters (non-greedy)
+a**!{2..4}		baaabbb		y	three "a" characters (explicit greed)
+a**:!{2..4}		baaabbb		y	three "a" characters (explicit greed)
+
+## Quantifier bare range
+.**2			a			n	only one character
+.**2			ab			y	two characters
+a**2			foobar		n	only one "a" character
+a**2			baabaa		y	two "a" characters
+a**0..4			bbbbbbb		y	no "a" characters
+a**2..4			bababab		n	not two consecutive "a" characters
+a**2..4			baabbbb		y	two "a" characters
+a**2..4			baaabbb		y	three "a" characters
+a**2..4			baaaabb		y	four "a" characters
+a**2..4			baaaaaa		y	four "a" characters
+a**2..*			baaaaaa		y	six "a" characters
+a**?2..*		baaaaaa		y	two "a" characters (non-greedy)
+a**:?2..*		baaaaaa		y	two "a" characters (non-greedy)
+a**!2..*		baaaaaa		y	six "a" characters (explicit greed)
+a**:!2..*		baaaaaa		y	six "a" characters (explicit greed)
+a**?2..4		baaabbb		y	two "a" characters (non-greedy)
+a**:?2..4		baaabbb		y	two "a" characters (non-greedy)
+a**!2..4		baaabbb		y	three "a" characters (explicit greed)
+a**:!2..4		baaabbb		y	three "a" characters (explicit greed)
+
+

commit 6bd4027dc43d5502417bb8a5d99e295b0f9e6630

[pge]: * Fix a bug introduced by r22944 that causes ':' to accept zero matches.

compilers/pge/PGE/Perl6Regex.pir
@@ -498,6 +498,18 @@ combinations.
     max = 1
     suffixpos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
 
+    if key == '**' goto quant_suffix
+    if key == ':' goto quant_cut
+    if key == '+' goto quant_max
+    ##  quantifier is '?' or '*'
+    min = 0
+  quant_max:
+    if key == '?' goto quant_suffix
+    ##  quantifier is '+' or '*'
+    max = PGE_INF
+    goto quant_suffix
+
+  quant_cut:
     #   The postfix:<:> operator may bring us here when it's really a
     #   term:<::> term.  So, we check for that here and fail this match
     #   if we really have a cut term.
@@ -505,7 +517,6 @@ combinations.
     $S0 = substr target, pos, 1
     if $S0 == ':' goto end
     mob['backtrack'] = PGE_BACKTRACK_NONE
-    goto quant_suffix_1
 
   quant_suffix:
     suffix = substr target, suffixpos, 2
@@ -529,14 +540,7 @@ combinations.
     pos = suffixpos + $I0
 
   quant:
-    if key == '**' goto quant_closure
-    if key == '+' goto quant_max
-    min = 0
-  quant_max:
-    if key == '?' goto quant_set
-    max = PGE_INF
-    goto quant_set
-
+    if key != '**' goto quant_set
   quant_closure:
     pos = find_not_cclass .CCLASS_WHITESPACE, target, pos, lastpos
     .local int isconst