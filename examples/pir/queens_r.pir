parrot-mirror/parrot

commit bb09ed3f6f520bc2401f482dcfb2d1a400b108c1

Finalize cleanup of examples/assembly. Move remaining examples into more appropriate dirs. As most of these remaining example are either not working or not easily testable, add only TODO tests for them.
Add some svn:keywords, such that $Id$ is expanded by SVN.


git-svn-id: https://svn.parrot.org/parrot/trunk@10183 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

examples/pir/queens_r.pir
@@ -1,6 +1,10 @@
+$Id$
+
 # recursive queens placement
 # prints all 92 solutions in chess notation
 # by leo
+
+# See also: pams/queens.pasm
 
 .sub main :main
     .local pmc queen_row, queen_state

MANIFEST
@@ -467,14 +467,6 @@ encodings/utf16.h                                 []
 encodings/utf8.c                                  []
 encodings/utf8.h                                  []
 examples/README                                   [main]doc
-examples/assembly/acorn.l                         [main]doc
-examples/assembly/hello-dwim.imc                  [main]doc
-examples/assembly/nanoforth.pasm                  [main]doc
-examples/assembly/nanoforth2.pasm                 [main]doc
-examples/assembly/ncurses_life.imc                [main]doc
-examples/assembly/pmcmops.pasm                    [main]doc
-examples/assembly/queens_r.imc                    [main]doc
-examples/assembly/thr-primes.imc                  [main]doc
 examples/benchmarks/addit.imc                     [main]doc
 examples/benchmarks/addit.pasm                    [main]doc
 examples/benchmarks/addit.pl                      [main]doc
@@ -587,7 +579,9 @@ examples/japh/japh6.pasm                          [main]doc
 examples/japh/japh7.pasm                          [main]doc
 examples/japh/japh8.pasm                          [main]doc
 examples/japh/japh9.pasm                          [main]doc
+examples/library/acorn.l                          [main]doc
 examples/library/getopt_demo.pir                  [main]doc
+examples/library/ncurses_life.imc                 [main]doc
 examples/library/md5sum.pir                       [main]doc
 examples/library/pcre.pir                         [main]doc
 examples/mops/README                              [main]doc
@@ -605,12 +599,14 @@ examples/pasm/cat.pasm                            [main]doc
 examples/pasm/fact.pasm                           [main]doc
 examples/pasm/hello.pasm                          [main]doc
 examples/pasm/lexical.pasm                        [main]doc
+examples/pasm/nanoforth.pasm                      [main]doc
+examples/pasm/nanoforth2.pasm                     [main]doc
 examples/pasm/queens.pasm                         [main]doc
 examples/pasm/small.xml                           [main]doc
 examples/pasm/stack.pasm                          [main]doc
 examples/pasm/trace.pasm                          [main]doc
 examples/pasm/xml_parser.pasm                     [main]doc
-examples/past/hello.past                          []
+examples/past/hello.past                          [main]doc
 examples/pge/README                               [main]doc
 examples/pge/all.pir                              [main]doc
 examples/pge/p6parse.pir                          [main]doc
@@ -619,13 +615,16 @@ examples/pge/simple.pir                           [main]doc
 examples/pir/circle.pir                           [main]doc
 examples/pir/euclid.pir                           [main]doc
 examples/pir/hanoi.pir                            [main]doc
+examples/pir/hello-dwim.pir                       [main]doc
 examples/pir/io.pir                               [main]doc
 examples/pir/life.pir                             [main]doc
 examples/pir/local_label.pir                      [main]doc
 examples/pir/mandel.pir                           [main]doc
+examples/pir/queens_r.pir                         [main]doc
 examples/pir/readline.pir                         [main]doc
 examples/pir/substr.pir                           [main]doc
 examples/pir/sudoku.pir                           [main]doc
+examples/pir/thr-primes.pir                       [main]doc
 examples/pir/uniq.pir                             [main]doc
 examples/pni/PQt.C                                [main]doc
 examples/pni/QtHelloWorld.pasm                    [main]doc

examples/README
@@ -1,10 +1,7 @@
 # $Id$
 
-The directory 'examples' provides examples of the proper,
+The directory 'examples' provides examples of proper,
 and sometimes hillarious, programs for Parrot.
-
-assembly  - Miscellaneous stuff in PASM and PIR
-            The examples will be moved to the dirs 'pir' and 'pasm'.
 
 benchmark - Contains benchmark. Run them with 'make testbench'
      

examples/benchmarks/mops.pasm
@@ -3,7 +3,7 @@
 
 =head1 NAME
 
-examples/benchmarks/mops.pasm - Calculate a benchmark
+examples/benchmarks/mops.pasm - Calculate a benchmark for Integer PMCs
 
 =head1 SYNOPSIS
 
@@ -65,7 +65,6 @@ DONE:   time   N5
 
 =head1 SEE ALSO
 
-F<examples/assembly/pmcmops.pasm>,
 F<examples/mops/mops.c>,
 F<examples/mops/mops.cs>,
 F<examples/mops/mops.il>,
@@ -77,4 +76,3 @@ F<examples/mops/mops.rb>,
 F<examples/mops/mops.scheme>.
 
 =cut
-

examples/library/ncurses_life.imc
@@ -1,13 +1,13 @@
-# Copyright (C) 2001-2003 The Perl Foundation.  All rights reserved.
+# Copyright (C) 2001-2005 The Perl Foundation.  All rights reserved.
 # $Id$
 
 =head1 NAME
 
-examples/assembly/ncurses_life.imc - Conway's Life (ncurses version)
+examples/library/ncurses_life.imc - Conway's Game of Life (ncurses version)
 
 =head1 SYNOPSIS
 
-    % ./parrot examples/assembly/ncurses_life.imc examples/assembly/acorn.l
+    % ./parrot examples/pasm/ncurses_life.imc examples/library/acorn.l
 
 =head1 DESCRIPTION
 
@@ -793,7 +793,7 @@ key_done:
 
 =head1 SEE ALSO
 
-F<examples/assembly/acorn.l>, F<examples/assembly/life.pasm>,
+F<examples/library/acorn.l>, F<examples/pasm/life.pasm>,
 F<library/ncurses.pasm>, F<library/ncurses.declarations>.
 
 =cut

examples/pasm/nanoforth.pasm
@@ -1,3 +1,5 @@
+# $Id$
+
 =pod
 
 =head1 nanoforth - a totally stripped down forth kernel

examples/pasm/nanoforth2.pasm
@@ -1,3 +1,5 @@
+# $Id$
+
 =pod
 
 =head1 nanoforth2 - a totally stripped down forth kernel

commit 015196ba66b6ae8b6f60e510572dbfa04feb3ca6

[examples] avoid a syntax error introduced in r10183

examples/pir/queens_r.pir
@@ -1,4 +1,4 @@
-$Id$
+# $Id$
 
 # recursive queens placement
 # prints all 92 solutions in chess notation