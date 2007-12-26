bb80cee851a720ecc1584ff781f7f76b5f474774
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-12-26 01:13:59 +0000

    Beginning to merge gen::cpu and auto::cpu.  Handled so far:  i386 and x86_64.

--- branches/cpu/config/gen/cpu/i386/auto.pm
+++ branches/cpu/config/gen/cpu/i386/auto.pm
@@ -48,6 +48,29 @@ sub runstep {
         }
         cc_clean();
     }
+
+    @files = qw( test_gcc_cmpxchg.in );
+    for my $f (@files) {
+        print " $f " if $verbose;
+        my ($suffix) = $f =~ /test_(\w+)/;
+        $f = "config/auto/cpu/i386/$f";
+        cc_gen($f);
+        eval { cc_build("-DPARROT_CONFIG_TEST") };
+        if ($@) {
+            print " $@ " if $verbose;
+        }
+        else {
+            if ( cc_run() =~ /ok/ ) {
+                $conf->data->set(
+                    "i386_has_$suffix" => '1',
+                    "HAS_i386_$suffix" => '1',
+                );
+                print " (\U$suffix) " if ($verbose);
+                $conf->data->add( ' ', TEMP_generated => $f );
+            }
+        }
+        cc_clean();
+    }
     return;
 }
 
