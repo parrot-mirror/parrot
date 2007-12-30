00787018c5cb81010a87f31332e1edf31180892e
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-12-30 04:18:58 +0000

    Tidying up the recent update.

--- branches/explicitconf/config/auto/cpu/i386/auto.pm
+++ branches/explicitconf/config/auto/cpu/i386/auto.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2007, The Perl Foundation.
+# Copyright (C) 2001-2006, The Perl Foundation.
 # $Id$
 
 =head1 NAME
@@ -7,7 +7,10 @@ config/auto/cpu/i386/auto.pm
 
 =head1 DESCRIPTION
 
-Test
+Test for MMX/SSE functionality. Creates these Config entries
+
+ TEMP_generated => 'files ...'   for inclusion in platform.c or platform.h
+ i386_has_mmx   => 1
 
 =cut
 
@@ -21,29 +24,52 @@ sub runstep {
 
     my $verbose = $conf->options->get('verbose');
 
-    my @files = qw( test_gcc_cmpxchg.in );
+    my @files = qw( memcpy_mmx.c memcpy_sse.c );
+    for my $f (@files) {
+        print " $f " if $verbose;
+        my ($suffix) = $f =~ /memcpy_(\w+)/;
+        my $path_f = "config/auto/cpu/i386/$f";
+        $conf->cc_gen($path_f);
+        eval( $conf->cc_build("-DPARROT_CONFIG_TEST") );
+        if ($@) {
+            print " $@ " if $verbose;
+        }
+        else {
+            if ( $conf->cc_run() =~ /ok/ ) {
+                _handle_cc_run_ok($conf, $suffix, $path_f, $verbose);
+            }
+        }
+        $conf->cc_clean();
+    }
+
+    @files = qw( test_gcc_cmpxchg.in );
     for my $f (@files) {
         print " $f " if $verbose;
         my ($suffix) = $f =~ /test_(\w+)/;
-        $f = "config/auto/cpu/i386/$f";
-        $conf->cc_gen($f);
+        my $path_f = "config/auto/cpu/i386/$f";
+        $conf->cc_gen($path_f);
         eval { $conf->cc_build("-DPARROT_CONFIG_TEST") };
         if ($@) {
             print " $@ " if $verbose;
         }
         else {
             if ( $conf->cc_run() =~ /ok/ ) {
-                $conf->data->set(
-                    "i386_has_$suffix" => '1',
-                    "HAS_i386_$suffix" => '1',
-                );
-                print " (\U$suffix) " if ($verbose);
-                $conf->data->add( ' ', TEMP_generated => $f );
+                _handle_cc_run_ok($conf, $suffix, $path_f, $verbose);
             }
         }
         $conf->cc_clean();
     }
     return;
+}
+
+sub _handle_cc_run_ok {
+    my ($conf, $suffix, $path_f, $verbose) = @_;
+    $conf->data->set(
+        "i386_has_$suffix" => '1',
+        "HAS_i386_$suffix" => '1',
+    );
+    print " (\U$suffix) " if ($verbose);
+    $conf->data->add( ' ', TEMP_generated => $path_f );
 }
 
 1;
