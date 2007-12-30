51d6a70883f3fc17ca24e3d820e3e61d4b4fd773
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2007-12-30 03:41:08 +0000

    Tagging trunk at r24283 so that the explicitconf can be synched to it.

---
+++ tags/explicitconf-24283/config/auto/cpu/x86_64/auto.pm
@@ -0,0 +1,44 @@
+# Copyright (C) 2004-2006, The Perl Foundation.
+# $Id$
+
+=head1 NAME
+
+config/auto/cpu/x86_64/auto.pm
+
+=head1 DESCRIPTION
+
+Adds -fPIC to x86_64 (AMD64) ccflags, if necessary.  Also handles
+cmpxchg.
+
+=cut
+
+package auto::cpu::x86_64::auto;
+
+use strict;
+use warnings;
+
+sub runstep {
+    my ( $self, $conf ) = @_;
+
+    my $ccflags = $conf->data->get('ccflags');
+    $conf->data->add( ' ', ccflags => "-fPIC" ) unless $ccflags =~ /-fPIC/;
+
+    # when running on gcc, we just define the config item
+    if ( defined( $conf->data->get('gccversion') ) ) {
+
+        # HAS_foo defines PARROT_HAS_`uc foo`
+        $conf->data->set(
+            "HAS_I386_GCC_CMPXCHG"   => '1',
+            "HAS_X86_64_GCC_CMPXCHG" => '1',
+        );
+    }
+}
+
+1;
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 100
+# End:
+# vim: expandtab shiftwidth=4:
