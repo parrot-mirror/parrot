765dd28e65be3a80d957d1edd0b995c6b7b3c125
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2008-01-27 03:00:03 +0000

    1. Added lib/Parrot/Harness/Smoke.pm as location to which smoke-test related code was refactored from t/harness. 2. Fine-tuning of lib/Parrot/Harness/DefaultTests.pm to increase testability: elimination of use of $FindBin::Bin; lists of test directories are now 'our' variables so they can be overridden in tests. 3. Added t/pharness/01-default_tests.t to test Parrot::Harness::DefaultTests. 4. config/gen/makefiles/root.in and lib/Parrot/Configure/Options/Test.pm:  Added t/pharness/ to lists of directories whose tests are run in 'perl Configure.pl --test', 'perl Configure.pl --test=build' and 'make buildtools_tests'.

--- branches/harness/lib/Parrot/Harness/DefaultTests.pm
+++ branches/harness/lib/Parrot/Harness/DefaultTests.pm
@@ -7,83 +7,84 @@ Parrot::Harness::DefaultTests - Tests run by default by F<t/harness>
 
 =head1 DESCRIPTION
 
-This file exports a single subroutine, C<get_default_tests()>, which is the
-list of tests run by F<t/harness> by default.
+This file exports by default a single subroutine, C<get_default_tests()>,
+which is the list of tests run by F<t/harness> by default.
 
-C<get_default_tests()> in list context returns the list of default tests.  In
+In list context, C<get_default_tests()> returns the list of default tests.  In
 scalar context it returns a reference to that list.
 
 =cut
 
 package Parrot::Harness::DefaultTests;
 use strict;
-use lib qw( . lib ../lib ../../lib );
-use FindBin qw/$Bin/;
 use base qw( Exporter );
 our @EXPORT = qw( get_default_tests );
 
+# runcore tests are always run.
+our @runcore_tests = qw(
+    t/compilers/imcc/*/*.t
+    t/op/*.t
+    t/pmc/*.t
+    t/oo/*.t
+    t/native_pbc/*.t
+    t/dynpmc/*.t
+    t/dynoplibs/*.t
+    t/compilers/pge/*.t
+    t/compilers/pge/p5regex/*.t
+    t/compilers/pge/perl6regex/*.t
+    t/compilers/tge/*.t
+    t/library/*.t
+);
+
+# core tests are run unless --runcore-tests is present.  Typically
+# this list and the list above are run in response to --core-tests
+our @core_tests = qw(
+    t/run/*.t
+    t/src/*.t
+    t/tools/*.t
+    t/perl/*.t
+    t/stm/*.t
+);
+
+# configure tests are tests to be run at the beginning of 'make test';
+# standard tests are other tests run by default with no core options
+# present
+our @configure_tests = qw( t/configure/*.t t/steps/*.t t/postconfigure/*.t );
+our @standard_tests = qw(
+    t/compilers/json/*.t
+    t/examples/*.t
+    t/doc/*.t
+    t/distro/manifest.t
+);
+
+our @developing_tests = (
+    't/distro/file_metadata.t',
+    map { "t/codingstd/$_" } qw(
+        c_code_coda.t
+        c_header_guards.t
+        c_indent.t
+        c_struct.t
+        check_toxxx.t
+        copyright.t
+        cppcomments.t
+        cuddled_else.t
+        filenames.t
+        gmt_utc.t
+        linelength.t
+        pccmethod_deps.t
+        pir_code_coda.t
+        svn_id.t
+        tabs.t
+        trailing_space.t
+    )
+);
+
 sub get_default_tests {
     my ($core_tests_only, $runcore_tests_only) = @_;
-
-    # runcore tests are always run.
-    my @runcore_tests = qw(
-        t/compilers/imcc/*/*.t
-        t/op/*.t
-        t/pmc/*.t
-        t/oo/*.t
-        t/native_pbc/*.t
-        t/dynpmc/*.t
-        t/dynoplibs/*.t
-        t/compilers/pge/*.t
-        t/compilers/pge/p5regex/*.t
-        t/compilers/pge/perl6regex/*.t
-        t/compilers/tge/*.t
-        t/library/*.t
-    );
-    
-    # core tests are run unless --runcore-tests is present.  Typically
-    # this list and the list above are run in response to --core-tests
-    my @core_tests = qw(
-        t/run/*.t
-        t/src/*.t
-        t/tools/*.t
-        t/perl/*.t
-        t/stm/*.t
-    );
-    
-    # configure tests are tests to be run at the beginning of 'make test';
-    # standard tests are other tests run by default with no core options
-    # present
-    my @configure_tests = qw( t/configure/*.t t/steps/*.t t/postconfigure/*.t );
-    my @standard_tests = qw(
-        t/compilers/json/*.t
-        t/examples/*.t
-        t/doc/*.t
-        t/distro/manifest.t
-    );
     
     # add metadata.t and coding standards tests only if we're DEVELOPING
-    if ( -e "$Bin/../DEVELOPING" ) {
-        push @standard_tests, 't/distro/file_metadata.t';
-        push @standard_tests, map { "t/codingstd/$_" } qw(
-            c_code_coda.t
-            c_header_guards.t
-            c_indent.t
-            c_struct.t
-            check_toxxx.t
-            copyright.t
-            cppcomments.t
-            cuddled_else.t
-            filenames.t
-            gmt_utc.t
-            linelength.t
-            pccmethod_deps.t
-            pir_code_coda.t
-            svn_id.t
-            tabs.t
-            trailing_space.t
-        );
-        # XXX: This takes WAY too long to run: perlcritic.t
+    if ( -e "DEVELOPING" ) {
+        push @standard_tests, @developing_tests;
     }
 
     # build the list of default tests
