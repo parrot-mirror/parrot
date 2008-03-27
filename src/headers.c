parrot-mirror/parrot

commit 20e86158393f1b0dde6f1f88ee9682d9eb145eb7

[GC] Minor speed improvements and consolidations.
git-svn-id: https://svn.parrot.org/parrot/trunk@26379 d31e2699-5ff4-0310-a27c-f18f2fbe73fe

src/headers.c
@@ -694,28 +694,34 @@ Parrot_forall_header_pools(PARROT_INTERP, int flag, ARGIN_NULLOK(void *arg),
 {
     Arenas * const arena_base = interp->arena_base;
 
-    if ((flag & (POOL_PMC | POOL_CONST)) == (POOL_PMC | POOL_CONST)) {
-        const int ret_val = (func)(interp, arena_base->constant_pmc_pool,
-                POOL_PMC | POOL_CONST, arg);
-        if (ret_val)
-            return ret_val;
-    }
     if (flag & POOL_PMC) {
-        const int ret_val = (func)(interp, arena_base->pmc_pool, POOL_PMC, arg);
-        if (ret_val)
-            return ret_val;
-    }
-    if ((flag & (POOL_BUFFER | POOL_CONST)) == (POOL_BUFFER | POOL_CONST)) {
-        const int ret_val = (func)(interp, arena_base->constant_string_header_pool,
-                POOL_BUFFER | POOL_CONST, arg);
+        Small_Object_Pool *pool = flag & POOL_CONST
+            ? arena_base->constant_pmc_pool
+            : arena_base->pmc_pool;
+
+        const int ret_val = (func)(interp, pool,
+            flag & (POOL_PMC | POOL_CONST) , arg);
+
         if (ret_val)
             return ret_val;
     }
 
+
     if (flag & POOL_BUFFER) {
-        int i;
-        for (i = 0; i < (INTVAL)interp->arena_base->num_sized; i++) {
+        INTVAL i;
+
+        if (flag & POOL_CONST) {
+            const int ret_val = (func)(interp,
+                arena_base->constant_string_header_pool,
+                POOL_BUFFER | POOL_CONST, arg);
+
+            if (ret_val)
+                return ret_val;
+        }
+
+        for (i = interp->arena_base->num_sized - 1; i >= 0; --i) {
             Small_Object_Pool * const pool = arena_base->sized_header_pools[i];
+
             if (pool) {
                 const int ret_val = (func)(interp, pool, POOL_BUFFER, arg);
                 if (ret_val)
@@ -723,6 +729,7 @@ Parrot_forall_header_pools(PARROT_INTERP, int flag, ARGIN_NULLOK(void *arg),
             }
         }
     }
+
     return 0;
 }
 

src/gc/dod.c
@@ -476,7 +476,8 @@ Parrot_dod_trace_pmc_data(PARROT_INTERP, ARGIN(PMC *p))
 
     if (data) {
         INTVAL i;
-        for (i = 0; i < PMC_int_val(p); i++)
+
+        for (i = PMC_int_val(p) - 1; i >= 0; --i)
             if (data[i])
                 pobject_lives(interp, (PObj *)data[i]);
     }

commit 48337aef316ec85faed32f04bdb67fd7bad1b365

[GC] Fixed a memory leak introduced in r26379.  In particular, the consolidation of Parrot_forall_header_pools() meant that the non-const PMC pool was never swept or freed during global destruction.

src/headers.c
@@ -832,6 +832,8 @@ Parrot_destroy_header_pools(PARROT_INTERP)
 #else
     const INTVAL start = 2;
 #endif
+
+    Parrot_forall_header_pools(interp, POOL_PMC, NULL, sweep_cb_pmc);
     Parrot_forall_header_pools(interp, POOL_PMC | POOL_CONST, NULL,
             sweep_cb_pmc);
 