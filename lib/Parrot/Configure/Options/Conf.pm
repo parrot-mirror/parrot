3a3ccd359f1639ecc2fda92656bde397111b1ba8
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2008-08-04 00:12:42 +0000

    [configure] To prepare for scripted configuration, move some code that is specific to handling of command-line options into Parrot::Configure::Options::Conf::CLI.  Adjust as needed in lib/Parrot/Configure/Options.pm, lib/Parrot/Configure/Options/Conf.pm and t/configure/001-options.t.

--- lib/Parrot/Configure/Options/Conf.pm
+++ lib/Parrot/Configure/Options/Conf.pm
@@ -7,110 +7,112 @@ use strict;
 use warnings;
 use base qw( Exporter );
 our @EXPORT_OK = qw(
-    @valid_options
     $script
-    %options_components
     $parrot_version
     $svnid
+    print_help
+    print_version
 );
+#    @valid_options
+#    %options_components
 use lib qw( lib );
 use Parrot::BuildUtil ();
 
-our @valid_options = qw{
-    ask
-    bindir
-    cage
-    cc
-    ccflags
-    ccwarn
-    cgoto
-    configure_trace
-    cxx
-    datadir
-    debugging
-    define
-    exec-prefix
-    execcapable
-    fatal
-    fatal-step
-    floatval
-    gc
-    help
-    icu-config
-    icuheaders
-    icushared
-    includedir
-    infodir
-    inline
-    intval
-    jitcapable
-    languages
-    ld
-    ldflags
-    lex
-    libdir
-    libexecdir
-    libs
-    link
-    linkflags
-    localstatedir
-    m
-    make
-    maintainer
-    mandir
-    miniparrot
-    nomanicheck
-    oldincludedir
-    opcode
-    ops
-    optimize
-    parrot_is_shared
-    pmc
-    prefix
-    profile
-    sbindir
-    sharedstatedir
-    silent
-    sysconfdir
-    test
-    verbose
-    verbose-step
-    version
-    without-gdbm
-    without-opengl
-    without-pcre
-    without-crypto
-    without-gettext
-    without-gmp
-    without-icu
-    yacc
-};
+#our @valid_options = qw{
+#    ask
+#    bindir
+#    cage
+#    cc
+#    ccflags
+#    ccwarn
+#    cgoto
+#    configure_trace
+#    cxx
+#    datadir
+#    debugging
+#    define
+#    exec-prefix
+#    execcapable
+#    fatal
+#    fatal-step
+#    floatval
+#    gc
+#    help
+#    icu-config
+#    icuheaders
+#    icushared
+#    includedir
+#    infodir
+#    inline
+#    intval
+#    jitcapable
+#    languages
+#    ld
+#    ldflags
+#    lex
+#    libdir
+#    libexecdir
+#    libs
+#    link
+#    linkflags
+#    localstatedir
+#    m
+#    make
+#    maintainer
+#    mandir
+#    miniparrot
+#    nomanicheck
+#    oldincludedir
+#    opcode
+#    ops
+#    optimize
+#    parrot_is_shared
+#    pmc
+#    prefix
+#    profile
+#    sbindir
+#    sharedstatedir
+#    silent
+#    sysconfdir
+#    test
+#    verbose
+#    verbose-step
+#    version
+#    without-gdbm
+#    without-opengl
+#    without-pcre
+#    without-crypto
+#    without-gettext
+#    without-gmp
+#    without-icu
+#    yacc
+#};
 
 our $script         = q{Configure.pl};
 our $parrot_version = Parrot::BuildUtil::parrot_version();
-our $svnid          = '$Id$',
+our $svnid          = '$Id$';
 
-my %short_circuits = (
-    help    => \&print_help,
-    version => \&print_version,
-);
-
-our %options_components = (
-    'valid_options'  => \@valid_options,
-    'script'         => $script,
-    'short_circuits' => \%short_circuits,
-    'conditionals'   => \&conditional_assignments,
-);
-
-sub conditional_assignments {
-    my $argsref = shift;
-    $argsref->{debugging} = 1
-        unless ( ( exists $argsref->{debugging} ) && !$argsref->{debugging} );
-    $argsref->{maintainer} = 1
-        if defined $argsref->{lex}
-            or defined $argsref->{yacc};
-    return $argsref;
-}
+#my %short_circuits = (
+#    help    => \&print_help,
+#    version => \&print_version,
+#);
+#
+#our %options_components = (
+#    'valid_options'  => \@valid_options,
+#    'script'         => $script,
+#    'short_circuits' => \%short_circuits,
+#    'conditionals'   => \&conditional_assignments,
+#);
+#
+#sub conditional_assignments {
+#    my $argsref = shift;
+#    $argsref->{debugging} = 1
+#        unless ( ( exists $argsref->{debugging} ) && !$argsref->{debugging} );
+#    $argsref->{maintainer} = 1
+#        if defined $argsref->{lex}
+#            or defined $argsref->{yacc};
+#    return $argsref;
+#}
 
 sub print_version {
     print "Parrot Version $parrot_version Configure 2.0\n";
