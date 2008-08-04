5f8855ae4087a8b3ab8b179fe03f609d6c78f2a4
jkeenan <jkeenan@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2008-08-04 00:14:18 +0000

    Delete commented-out code.

--- lib/Parrot/Configure/Options/Conf.pm
+++ lib/Parrot/Configure/Options/Conf.pm
@@ -1,6 +1,5 @@
 # Copyright (C) 2007-2008, The Perl Foundation.
 # $Id$
-
 package Parrot::Configure::Options::Conf;
 
 use strict;
@@ -13,106 +12,12 @@ our @EXPORT_OK = qw(
     print_help
     print_version
 );
-#    @valid_options
-#    %options_components
 use lib qw( lib );
 use Parrot::BuildUtil ();
-
-#our @valid_options = qw{
-#    ask
-#    bindir
-#    cage
-#    cc
-#    ccflags
-#    ccwarn
-#    cgoto
-#    configure_trace
-#    cxx
-#    datadir
-#    debugging
-#    define
-#    exec-prefix
-#    execcapable
-#    fatal
-#    fatal-step
-#    floatval
-#    gc
-#    help
-#    icu-config
-#    icuheaders
-#    icushared
-#    includedir
-#    infodir
-#    inline
-#    intval
-#    jitcapable
-#    languages
-#    ld
-#    ldflags
-#    lex
-#    libdir
-#    libexecdir
-#    libs
-#    link
-#    linkflags
-#    localstatedir
-#    m
-#    make
-#    maintainer
-#    mandir
-#    miniparrot
-#    nomanicheck
-#    oldincludedir
-#    opcode
-#    ops
-#    optimize
-#    parrot_is_shared
-#    pmc
-#    prefix
-#    profile
-#    sbindir
-#    sharedstatedir
-#    silent
-#    sysconfdir
-#    test
-#    verbose
-#    verbose-step
-#    version
-#    without-gdbm
-#    without-opengl
-#    without-pcre
-#    without-crypto
-#    without-gettext
-#    without-gmp
-#    without-icu
-#    yacc
-#};
 
 our $script         = q{Configure.pl};
 our $parrot_version = Parrot::BuildUtil::parrot_version();
 our $svnid          = '$Id$';
-
-#my %short_circuits = (
-#    help    => \&print_help,
-#    version => \&print_version,
-#);
-#
-#our %options_components = (
-#    'valid_options'  => \@valid_options,
-#    'script'         => $script,
-#    'short_circuits' => \%short_circuits,
-#    'conditionals'   => \&conditional_assignments,
-#);
-#
-#sub conditional_assignments {
-#    my $argsref = shift;
-#    $argsref->{debugging} = 1
-#        unless ( ( exists $argsref->{debugging} ) && !$argsref->{debugging} );
-#    $argsref->{maintainer} = 1
-#        if defined $argsref->{lex}
-#            or defined $argsref->{yacc};
-#    return $argsref;
-#}
 
 sub print_version {
     print "Parrot Version $parrot_version Configure 2.0\n";
