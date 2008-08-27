0c341f3ad72a103e6f9d1f0ba088cfa328359205
moritz <moritz@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2008-08-27 18:31:51 +0000

    * Finished implementation of the makefile expansion - "perl-style" * Changed all relevant makefile templates. rurban++, patch courtesy by Reini Urban

--- branches/cygwin070patches/lib/Parrot/Configure/Compiler.pm
+++ branches/cygwin070patches/lib/Parrot/Configure/Compiler.pm
@@ -33,6 +33,26 @@ use Parrot::Configure::Utils qw(
     _run_command _build_compile_command
     move_if_diff
 );
+my $DEBUG;
+
+our %file_types_info = (
+    makefile => {
+        comment_type    => '#',
+        vim_ft          => 'make',
+    },
+    c => {
+        comment_type    => '/*',
+        vim_ft          => 'c',
+    },
+    pmc => {
+        comment_type    => '/*',
+        vim_ft          => 'pmc',
+    },
+    perl => {
+        comment_type    => '#',
+        vim_ft          => 'perl',
+    },
+);
 
 =item C<cc_gen()>
 
@@ -46,7 +66,7 @@ sub cc_gen {
     my $conf   = shift;
     my $source = shift;
 
-    $conf->genfile( $source, "test_$$.c" );
+    $conf->genfile( $source, "test_$$.c", file_type => 'none' );
 }
 
 =item C<cc_build()>
@@ -181,47 +201,70 @@ replacement syntax assumes the source text is on a single line.)
 
 =over 4
 
-=item makefile
+=item file_type
 
-If set to a true value, this flag sets (unless overridden) C<comment_type>
-to '#', C<replace_slashes> to enabled, and C<conditioned_lines> to enabled.
+If set to a C<makefile>, C<c> or C<perl> value, C<comment_type> will be set
+to corresponding value.
+Moreover, when set to a C<makefile> value, it will set C<replace_slashes> to
+enabled, and C<conditioned_lines> to enabled.
 
-If the name of the file being generated ends in C<Makefile>, this option
-defaults to true.
+Its value will be detected automatically by target file name unless you set
+it to a special value C<none>.
 
-=item conditioned_lines #+(): #-():
+=item conditioned_lines #IF #UNLESS #ELSIF #ELSE
 
-If conditioned_lines is true, then lines in the file that begin with
-C<#+(expr):> are skipped if the expr condition is false.
-Lines that begin with C<#-(var):> are skipped if the expr condition is true.
+If conditioned_lines is true, then several expressions at the first column
+are evaluated and replaced by the content after the C<:> dependending on
+the evaluation of the expression.
+
+Lines beginning with C<#IF(expr):> are skipped if the expr
+condition is false, otherwise the content after the C<:> is inserted.
+Lines beginning with C<#UNLESS(expr):> are skipped if the expr condition
+is true.
+Lines beginning with C<#ELSIF(expr):> are evaluated if the former C<#IF(expr):>
+evaluated to false.
+Lines beginning with C<#ELSE:> are evaluated if the former C<#IF(expr):> or
+C<#ELSIF(expr):> evaluated to false.
+
 For legacy the old syntax #CONDITIONED_LINE(var): and
 #INVERSE_CONDITIONED_LINE(var): is also supported.
+For lispers the well-known reader-macro syntax #+(and key1 (not key2)):
+is also supported, #+ #- with and, or and not, or being the default
+for multiple keys without operator. But note the ending C<:>.
 
-A condition expr may be a single keyword, which is true if a config key is true
-or equal to the platform name  (case-sensitive),
-or a logical combination of AND, OR and NOT in case-insensitive LISP syntax
-  like (AND var1 var2...) or (OR var1 var2...) or (NOT var1...)
-  as in the common lisp reader, 'OR' being the default for multiple keys.
-Multiple keys are space seperated.
+A condition expr may be:
 
-Keys may also consist of key=value pairs, where the key is checked for
-equalness to the value. Note that values may contain no spaces here.
-TODO: support quotes in values
+  * a single key, which is true if a config key is true
+    - not 0 and not undef and not an empty string,
+  * or equal to the platform name, the perl5 osname - case-sensitive,
+  * or a key=value expression, which checks for the config key,
+    Note that values may contain no spaces here. Quotes in values
+    are not supported.
+  * or a logical combination of C<|> (or), C<&> (and) and C<!> (not),
+    with the following order of precedence:  ! & |
 
-  #+(var1 var2...) defaults to #+(or var1 var2...)
-  #-(var1 var2...) defaults to #-(or var1 var2...)
+A key must only consist of word characters, and is checked
+case-sensitively against the config key or the platform name.
 
 For instance:
 
-  #+(win32): $(SRC_DIR)/atomic/gcc_x86$(O)
+  #IF(win32): $(SRC_DIR)/atomic/gcc_x86$(O)
 
 will be processed if the platform is win32.
 
-  #-(win32 cygwin): $(SRC_DIR)/atomic/gcc_x86$(O)
+  #IF(cpuarch=i386): $(SRC_DIR)/atomic/gcc_x86$(O)
 
-will be skipped if the platform is win32 or cygwin.
+will be skipped if the value of the config key "cpuarch" is not "i386".
 
-  #+(and win32 glut (not cygwin)):
+  #IF(cpuarch=i386): $(SRC_DIR)/atomic/gcc_x86$(O)
+  #ELSIF(cpuarch=sparcv9): $(SRC_DIR)/atomic/sparc_v9.s
+  #ELSE:
+
+will use " $(SRC_DIR)/atomic/gcc_x86$(O)" on cpuarch=i386,
+" $(SRC_DIR)/atomic/sparc_v9.s" on cpuarch=sparcv9
+and print an empty line otherwise.
+
+  #IF(win32 & glut & !cygwin):
 
 will be used on win32 and if glut is defined, but not on cygwin.
 
@@ -299,20 +342,49 @@ sub genfile {
     open my $in,  '<', $source       or die "Can't open $source: $!";
     open my $out, '>', "$target.tmp" or die "Can't open $target.tmp: $!";
 
-    if ( !exists $options{makefile} && $target =~ m/makefile$/i ) {
-        $options{makefile} = 1;
+    if ( !exists $options{file_type}) {
+        if ( $target =~ m/makefile$/i ) {
+            $options{file_type} = 'makefile';
+        }
+        elsif ($target =~ m/\.pl$/i ) {
+            $options{file_type} = 'perl';
+        }
+        elsif ($target =~ m/\.[hc]$/ ) {
+            $options{file_type} = 'c';
+        }
+        elsif ($target =~ m/\.pmc$/ ) {
+            $options{file_type} = 'pmc';
+        }
+    } elsif ( $options{file_type} eq 'none' ) {
+        delete $options{file_type};
     }
 
-    if ( $options{makefile} ) {
-        exists $options{comment_type}      or $options{comment_type}      = '#';
-        exists $options{replace_slashes}   or $options{replace_slashes}   = 1;
-        exists $options{conditioned_lines} or $options{conditioned_lines} = 1;
+    if ( $options{file_type} ) {
+        unless ( exists $file_types_info{$options{file_type}} ) {
+            die "Unknown file_type '$options{file_type}'";
+        }
+        unless ( exists $options{comment_type} ) {
+            $options{comment_type} =
+                $file_types_info{$options{file_type}}{comment_type};
+        }
+        if ( $options{file_type} eq 'makefile' ) {
+            $options{replace_slashes}   = 1;
+            $options{conditioned_lines} = 1;
+        }
     }
 
     if ( $options{comment_type} ) {
-        my @comment = ( 'ex: set ro ft=c:',
+        my @comment = ( 'ex: set ro',
             'DO NOT EDIT THIS FILE',
             'Generated by ' . __PACKAGE__ . " from $source" );
+
+        if ($options{file_type}) {
+            $comment[0] .=
+                ' ft=' . $file_types_info{$options{file_type}}{vim_ft} . ':';
+        }
+        else {
+            $comment[0] .= ':';
+        }
 
         if ( $options{comment_type} eq '#' ) {
             foreach my $line (@comment) {
@@ -335,6 +407,7 @@ sub genfile {
     # this loop can not be implemented as a foreach loop as the body
     # is dependent on <IN> being evaluated lazily
 
+    my $former_truth = 99;
     while ( my $line = <$in> ) {
 
         # everything after the line starting with #perl is eval'ed
@@ -354,21 +427,50 @@ sub genfile {
             last;
         }
         if ( $options{conditioned_lines} ) {
+            my ($op, $expr, $rest);
             # allow multiple keys and nested parens here
-            if ( $line =~ m/^#([-+])\((.+)\):(.*)/s ) {
-          my $truth = cond_eval($conf, $2);
-              next if ($1 eq '-') and $truth;
-                next if ($1 eq '+') and not $truth;
-                $line = $3;
+            if (($op,$expr,$rest)=($line =~ m/^#(IF|UNLESS|ELSIF)\((.+)\):(.*)/s)) {
+                if (($op eq 'ELSIF') and $former_truth) {
+                    next;  # no useless check if former IF was true
+                }
+                my $truth = cond_eval($conf, $expr);
+                if ($op eq 'IF') {
+                    $former_truth = $truth;
+                    next unless $truth;
+                }
+                elsif ($op eq 'UNLESS') {
+                    $former_truth = !$truth;
+                    next if $truth;
+                }
+                elsif ($op eq 'ELSIF') {
+                    $former_truth = $truth;
+                    next unless $truth;
+                }
+                $line = $rest;
             }
-      # but here not (legacy)
-        elsif ( $line =~ m/^#CONDITIONED_LINE\(([^)]+)\):(.*)/s ) {
-                next unless cond_eval($conf, $1);
-                $line = $2;
+            elsif ( $former_truth != 99 and $line =~ m/^#ELSE:(.*)/s ) {
+                next if $former_truth;
+                $line = $1;
             }
-            elsif ( $line =~ m/^#INVERSE_CONDITIONED_LINE\(([^)]+)\):(.*)/s ) {
-                next if cond_eval($conf, $1);
-                $line = $2;
+            # lisp-style syntax
+            elsif (($op,$expr,$rest)=($line =~ m/^#([-+])\((.+)\):(.*)/s)) {
+                my $truth = cond_eval_lisp($conf, $expr);
+                next if ($op eq '-') and $truth;
+                next if ($op eq '+') and not $truth;
+                $line = $rest;
+            }
+            # legacy
+            elsif (($expr,$rest)=($line =~ m/^#CONDITIONED_LINE\(([^)]+)\):(.*)/s)) {
+                next unless cond_eval($conf, $expr);
+                $line = $rest;
+            }
+            elsif (($expr,$rest)=($line =~ m/^#INVERSE_CONDITIONED_LINE\(([^)]+)\):(.*)/s )) {
+                next if cond_eval($conf, $expr);
+                $line = $rest;
+            }
+
+            else { # reset
+                $former_truth = 99; # ELSE must immediately follow a conditional.
             }
         }
 
@@ -467,32 +569,110 @@ sub genfile {
     move_if_diff( "$target.tmp", $target, $options{ignore_pattern} );
 }
 
-# Return the next subexpression from the expression in $_[0] and
-# remove it from the input expression.
-# E.g. "and (not win32) has_glut"
-#      => and => (not win32) => has_glut
+# Return the next subexpression from the expression in $_[0]
+# and remove it from the input expression.
+# E.g. "and (not win32) has_glut" - lisp-style
+#        => and => (not win32) => has_glut
+#      "(!win32&has_glut)|cygwin"   - perl-style
+#        !win32&has_glut => !win32 => &has_glut => |cygwin
 sub next_expr {
     my $s = $_[0];
     return "" unless $s;
-    if ($s =~ /^\((.+)\)\s*(.*)/) { # longest match to matching closing paren
-        $_[0] = $2 ? $2 : "";       # modify the 2nd arg
-        #print "** \"$s\" => (\"$1\",\"$_[0]\")\n";
+    # start of a subexpression?
+    if ($s =~ /^\((.+)\)\s*(.*)/o) {    # longest match to matching closing paren
+        $_[0] = $2 ? $2 : "";           # modify the 2nd arg
+        print "#** nextsub \"$s\" => (\"$1\",\"$_[0]\")\n";
         return $1;
     }
     else {
         $s =~ s/^\s+//;                 # left-trim to make it more robust
-        $s =~ m/^([^ \(]+)\s*(.*)?/;    # shortest match to next whitespace or open paren
-        $_[0] = $2 ? $2 : "";           # modify the 2nd arg
-        #print "** \"$s\" => (\"$1\",\"$_[0]\")\n";
-        return $1;
+        if ($s =~ m/^([\w=]+)\s*(.*)?/o) { # shortest match to next non-word char
+            # start with word expr
+            $_[0] = $2 ? $2 : "";       # modify the 2nd arg expr in the caller
+            print "#** nextexpr \"$s\" => (\"$1\",\"$_[0]\")\n" if $DEBUG;
+            return $1;
+        }
+        else {
+            # special case: start with non-word op (perl-syntax only)
+            $s =~ m/^([|&!])\s*(.*)?/o; # shortest match to next word char
+            $_[0] = $2 ? $2 : "";       # modify the 2nd arg expr in the caller
+            print "#** nextop \"$s\" => (\"$1\",\"$_[0]\")\n" if $DEBUG;
+            return $1;
+        }
     }
 }
 
-# Recursively evaluate AND, OR, NOT for multiple keys as LISP expressions.
 # Checks the logical truth of the hash value: exists and not empty.
 # Also check the platform name if the hash key does not exist.
 # Also check for key=value, like #+(ld=gcc)
+sub cond_eval_single {
+    my $conf = $_[0];
+    my $key  = $_[1];
+    return unless defined $key;
+    if ($key =~ /^(\w+)=(.+)$/) {
+        return ($2 eq $conf->data->get($1));
+    }
+    else {
+        return exists($conf->data->{c}->{$key})
+            ? ($conf->data()->get($key) ? 1 : 0)
+            : $key eq $conf->data()->get('osname');
+    }
+}
+
+# Recursively evaluate boolean expressions with multiple keys and | & ! ops.
+# Order of precedence: ! & | (NOT, AND, OR)
 sub cond_eval {
+    my $conf = $_[0];
+    my $expr = $_[1];
+    my @count = split /[\s!&|]+/, $expr;
+    if (@count > 1) { # multiple keys: recurse into
+        my $truth = 0;
+        if (substr($expr,0,1) eq '!') { # NOT is the only op which may start an expr
+            print "#* ! \"$expr\"\n" if $DEBUG;
+            return !cond_eval($conf, substr($expr,1));
+        }
+        else {
+            my $key = next_expr($expr);
+            my $op  = '';
+            while ($key) {
+                print "#* op=\"$op\" key=\"$key\"" if $DEBUG;
+                if ($key eq '!' or uc($key) eq 'NOT') {
+                    $key = next_expr($expr);
+                    print "\n#* op=\"!\" key=\"$key\"" if $DEBUG;
+                    $truth = !cond_eval($conf, $key);
+                }
+                else {
+                    # log OR shortcut if already $truth
+                    last if $truth and ($op eq '|' or uc($op) eq 'OR');
+                    $truth = cond_eval($conf, $key);
+                }
+                print " => ",$truth?'true':'false'," rest=\"$expr\"\n" if $DEBUG;
+                if ($op eq '!'  or uc($op) eq 'NOT') {
+                    $truth = $truth ? 0 : 1;
+                }
+                elsif ($op eq '&' or uc($op) eq 'AND') {
+                    last unless $truth; # skip on early fail
+                }
+                my $prevexpr = $expr;
+                $op  = next_expr($expr);
+                if ($op) {
+                    $key = next_expr($expr);
+                }
+                elsif($prevexpr) {
+                    die "Makefile syntax error: missing op in \"$_[1]\" at \"$prevexpr\".\n";
+                }
+                else {
+                    last; # end of expr, nothing left
+                }
+            }
+        }
+        return $truth;
+    }
+    cond_eval_single($conf, $expr);
+}
+
+# Recursively evaluate boolean AND, OR and NOT lisp expressions for multiple keys.
+sub cond_eval_lisp {
     my $conf = $_[0];
     my $expr = $_[1];
     my $key = $expr;
@@ -510,21 +690,14 @@ sub cond_eval {
         }
         while ($key) {
             last if $truth and ($op eq 'or'); # logical shortcut on OR and already $truth
-            $truth = cond_eval($conf, $key);
+            $truth = cond_eval_lisp($conf, $key);
             if    ($op eq 'not') { $truth = $truth ? 0 : 1; }
             elsif ($op eq 'and') { last unless $truth; } # skip on early fail
             $key = next_expr($expr);
         }
         return $truth;
     }
-    if ($key =~ /^(\w+)=(.+)$/) {
-        return $conf->data->get($1) eq $2;
-    }
-    else {
-        return exists($conf->data->{c}->{$key})
-               ? $conf->data()->get($key)
-               : $key eq $^O;
-  }
+    cond_eval_single($conf, $key);
 }
 
 sub append_configure_log {
