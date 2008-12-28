a506aa1f8353b393a9466e71122c205cdd1b6d24
rurban <rurban@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2008-12-28 12:51:39 +0000

    58034-config_args

--- branches/pdd30install_stage3/Configure.pl
+++ branches/pdd30install_stage3/Configure.pl
@@ -63,6 +63,8 @@ $conf->add_steps( @{ $steps_list_ref } );
 
 # from Parrot::Configure::Data
 $conf->options->set( %{$args} );
+# save the command-line
+$conf->data->set(config_args => join(" ", @ARGV));
 
 # Log files created by Configure.pl in MANIFEST.configure.generated
 $conf->{active_configuration} = 1;
