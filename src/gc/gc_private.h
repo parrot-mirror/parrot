f65d830cd650bd486a0d6d8a0e39da46fd642756
bacek <bacek@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2010-05-30 14:55:49 +0000

    Add GC_Sys.is_pmc_ptr.

--- src/gc/gc_private.h
+++ src/gc/gc_private.h
@@ -123,6 +123,7 @@ typedef struct GC_Subsystem {
     Buffer* (*allocate_bufferlike_header)(PARROT_INTERP, size_t size);
     void    (*free_bufferlike_header)(PARROT_INTERP, Buffer*, size_t size);
 
+    int  (*is_pmc_ptr)(PARROT_INTERP, void*);
     void (*mark_pobj_header)(PARROT_INTERP, PObj*);
     void (*mark_pmc_header)(PARROT_INTERP, PMC *);
 
