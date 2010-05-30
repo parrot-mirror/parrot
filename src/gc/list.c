728247005ebdbe77c2eb22a4be7eb1f3b0ed577a
bacek <bacek@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2010-05-30 09:38:27 +0000

    Add stub for double-linked lists

---
+++ src/gc/list.c
@@ -0,0 +1,49 @@
+/*
+Copyright (C) 2010, Parrot Foundation.
+$Id$
+
+=head1 NAME
+
+src/gc/list.c - Implementation of double linked lists.
+
+=head1 DESCRIPTION
+
+This code implements double linked list of GCable objects.
+
+=cut
+
+*/
+
+#include "parrot/parrot.h"
+#include "list.h"
+
+/* HEADERIZER HFILE: src/gc/list.h */
+
+/* HEADERIZER BEGIN: static */
+/* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
+
+/* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
+/* HEADERIZER END: static */
+
+/*
+
+=over 4
+
+=cut
+
+*/
+
+/*
+
+=back
+
+=cut
+
+*/
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
---
+++ src/gc/list.h
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2010, Parrot Foundation.
+$Id$
+
+=head1 NAME
+
+src/gc/list.h - Linked lists of allocated objects.
+
+=head1 DESCRIPTION
+
+Implementation of double linked lists used by various GC implementations.
+
+*/
+
+#ifndef PARROT_GC_LIST_H_GUARD
+#define PARROT_GC_LIST_H_GUARD
+
+
+/* HEADERIZER BEGIN: src/gc/list.c */
+/* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
+
+/* Don't modify between HEADERIZER BEGIN / HEADERIZER END.  Your changes will be lost. */
+/* HEADERIZER END: src/gc/list.c */
+
+#endif /* PARROT_GC_LIST_H_GUARD */
+
+/*
+ * Local variables:
+ *   c-file-style: "parrot"
+ * End:
+ * vim: expandtab shiftwidth=4:
+ */
