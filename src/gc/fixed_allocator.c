d9ebfd1bd4a520cd450c4a1de1018090f7d6000b
mikehh <mikehh@d10d2e30-fb94-4b0b-9bf1-c816d7a57048>
2010-06-08 12:40:50 +0000

    add ASSERT_ARGS and fix line length

--- src/gc/fixed_allocator.c
+++ src/gc/fixed_allocator.c
@@ -57,6 +57,8 @@ PARROT_CAN_RETURN_NULL
 struct Fixed_Allocator*
 Parrot_gc_fixed_allocator_new(PARROT_INTERP)
 {
+    ASSERT_ARGS(Parrot_gc_fixed_allocator_new)
+
     return mem_internal_allocate_zeroed_typed(Fixed_Allocator);
 }
 
@@ -64,6 +66,8 @@ PARROT_EXPORT
 void
 Parrot_gc_fixed_allocator_destroy(PARROT_INTERP, ARGFREE_NOTNULL(Fixed_Allocator *allocator))
 {
+    ASSERT_ARGS(Parrot_gc_fixed_allocator_destroy)
+
 }
 
 PARROT_EXPORT
@@ -73,6 +77,8 @@ Parrot_gc_fixed_allocator_allocate(PARROT_INTERP,
         ARGIN(Fixed_Allocator *allocator),
         size_t size)
 {
+    ASSERT_ARGS(Parrot_gc_fixed_allocator_allocate)
+
     /* We always align size to 4/8 bytes. */
     size_t  index, alloc_size;
     void   *ret;
@@ -84,7 +90,8 @@ Parrot_gc_fixed_allocator_allocate(PARROT_INTERP,
         size_t new_size = index + 1;
         /* (re)allocate pools */
         if (allocator->num_pools)
-            allocator->pools = mem_internal_realloc_n_zeroed_typed(allocator->pools, new_size, allocator->num_pools, Pool_Allocator*);
+            allocator->pools = mem_internal_realloc_n_zeroed_typed(
+                                allocator->pools, new_size, allocator->num_pools, Pool_Allocator*);
         else
             allocator->pools = mem_internal_allocate_n_zeroed_typed(new_size, Pool_Allocator*);
 
@@ -105,6 +112,8 @@ Parrot_gc_fixed_allocator_free(PARROT_INTERP,
         ARGIN(Fixed_Allocator *allocator),
         ARGFREE_NOTNULL(void *data), size_t size)
 {
+    ASSERT_ARGS(Parrot_gc_fixed_allocator_free)
+
     /* We always align size to 4/8 bytes. */
     size_t index = (size - 1) / sizeof (void*);
 
